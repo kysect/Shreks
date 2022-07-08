4af52564e56692448f8b8a7068a1934fd9fef776
Fredi Kats <fredikats@outlook.com>
2022-07-08 20:29:55 +0400

    Add projects and basic interfaces

---
+++ Source/Domain/Kysect.Shreks.Abstractions/IGithubActivityHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace Kysect.Shreks.Abstractions;
+
+public record PullRequestCreateEventArguments();
+public record PullRequestNewCommitEventArguments();
+public record PullRequestReviewAddedEventArguments();
+public record PullRequestCommandSendEventArguments();
+
+public interface IGithubActivityHandler
+{
+    void HandlePullRequestCreation(PullRequestCreateEventArguments arguments);
+    void HandlePullRequestNewCommit(PullRequestNewCommitEventArguments arguments);
+    void HandlePullRequestReviewAdded(PullRequestReviewAddedEventArguments arguments);
+    void HandlePullRequestCommandSend(PullRequestCommandSendEventArguments arguments);
+}
---
+++ Source/Domain/Kysect.Shreks.Abstractions/IGoogleTableAccessor.cs
@@ -0,0 +1,16 @@
+﻿namespace Kysect.Shreks.Abstractions;
+
+public record SubjectHomeworkInfo();
+public record GroupQueue();
+
+public record AddStudentSubmitArgument();
+public record AddStudentToQueueArguments();
+
+
+public interface IGoogleTableAccessor
+{
+    SubjectHomeworkInfo ReadSubjectHomeworkInfo();
+    GroupQueue ReadGroupQueue();
+    void AddStudentSubmit(AddStudentSubmitArgument arguments);
+    void AddStudentToQueue(AddStudentToQueueArguments arguments);
+}
