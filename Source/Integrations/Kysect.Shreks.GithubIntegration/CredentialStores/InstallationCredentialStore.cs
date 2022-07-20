83d404907494ec4878b2740e012993c065a580c9
Vladislav Grigoriev <vvllaadd99@yandex.ru>
2022-07-20 12:06:37 +0300

    refactor InstallationCredentialStore.cs

--- Source/Integrations/Kysect.Shreks.GithubIntegration/CredentialStores/InstallationCredentialStore.cs
+++ Source/Integrations/Kysect.Shreks.GithubIntegration/CredentialStores/InstallationCredentialStore.cs
@@ -4,12 +4,45 @@ namespace Kysect.Shreks.GithubIntegration.CredentialStores;
 
 public class InstallationCredentialStore : ICredentialStore
 {
-    private enum TokenRefreshState
+    private struct TokenRefreshState
     {
-        NotRefreshing,
-        StartRefreshing,
-        Refreshing
+        private enum State
+        {
+            NotRefreshing, //refreshing task is not running
+            StartRefreshing, //refreshing task is being created
+            Refreshing //refreshing task is running
+        }
+        
+        private volatile int _state;
+
+        public TokenRefreshState()
+        {
+            _state = (int)State.NotRefreshing;
+        }
+        
+        public bool IsRefreshing()
+        {
+            return _state == (int)State.Refreshing;
+        }
+
+        public void SetNotRefreshing()
+        {
+            _state = (int)State.NotRefreshing;
+        }
+
+        public bool TrySetStartRefresing()
+        {
+            return Interlocked.CompareExchange(ref _state, (int)State.StartRefreshing,
+                (int)State.NotRefreshing) == (int)State.NotRefreshing;
+        }
+        public bool TrySetRefresing()
+        {
+            return Interlocked.CompareExchange(ref _state, (int)State.Refreshing,
+                (int)State.StartRefreshing) == (int)State.StartRefreshing;
+        }
     }
+    
+    
 
     private readonly IGitHubClient _gitHubAppClient;
     private readonly long _installationId;
@@ -17,60 +50,63 @@ public class InstallationCredentialStore : ICredentialStore
     private long _expirationUnixTimestamp;
     private volatile Task<Credentials> _task;
 
-    private volatile int _tokenRefreshState;
+    private TokenRefreshState _tokenRefreshState;
 
     public InstallationCredentialStore(IGitHubClient gitHubAppClient, long installationId)
     {
         _gitHubAppClient = gitHubAppClient;
         _installationId = installationId;
-        _tokenRefreshState = (int)TokenRefreshState.NotRefreshing;
+        _tokenRefreshState = new TokenRefreshState();
         _expirationUnixTimestamp = 0;
         _task = Task.FromResult<Credentials>(null!);
     }
 
-    private async Task<Credentials> GetTask()
-    {
-        try
-        {
-            var token = await _gitHubAppClient.GitHubApps.CreateInstallationToken(_installationId);
-            Volatile.Write(ref _expirationUnixTimestamp, token.ExpiresAt.AddMinutes(-1).ToUnixTimeSeconds());
-            return new Credentials(token.Token);
-        }
-        catch
-        {
-            Volatile.Write(ref _expirationUnixTimestamp, DateTimeOffset.Now.ToUnixTimeSeconds());
-            throw;
-        }
-        finally
-        {
-            _tokenRefreshState = (int)TokenRefreshState.NotRefreshing;
-        }
-    }
-
     public Task<Credentials> GetCredentials()
     {
-        if (Volatile.Read(ref _expirationUnixTimestamp) <= DateTimeOffset.Now.ToUnixTimeSeconds() &&
-            _tokenRefreshState != (int)TokenRefreshState.Refreshing)
+        if (!IsTokenExpired() || _tokenRefreshState.IsRefreshing()) return _task;
+        lock (this)
         {
-            lock (this)
+            if (!_tokenRefreshState.TrySetStartRefresing()) return _task;
+            if (IsTokenExpired())
             {
-                if (Interlocked.CompareExchange(ref _tokenRefreshState, (int)TokenRefreshState.StartRefreshing,
-                        (int)TokenRefreshState.NotRefreshing) == (int)TokenRefreshState.NotRefreshing)
-                {
-                    if (Volatile.Read(ref _expirationUnixTimestamp) <= DateTimeOffset.Now.ToUnixTimeSeconds())
-                    {
-                        _task = GetTask();
-                        Interlocked.CompareExchange(ref _tokenRefreshState, (int)TokenRefreshState.Refreshing,
-                            (int)TokenRefreshState.StartRefreshing);
-                    }
-                    else
-                    {
-                        _tokenRefreshState = (int)TokenRefreshState.NotRefreshing;
-                    }
-                }
+                _task = RefreshToken();
+                _tokenRefreshState.TrySetRefresing();
+            }
+            else
+            {
+                _tokenRefreshState.SetNotRefreshing();
             }
         }
 
         return _task;
+    }
+    
+    private async Task<Credentials> RefreshToken()
+    {
+        try
+        {
+            var token = await _gitHubAppClient.GitHubApps.CreateInstallationToken(_installationId);
+            SetExpirationDate(token.ExpiresAt.AddMinutes(-1));
+            return new Credentials(token.Token);
+        }
+        catch
+        {
+            SetExpirationDate(DateTimeOffset.Now);
+            throw;
+        }
+        finally
+        {
+            _tokenRefreshState.SetNotRefreshing();
+        }
+    }
+    
+    private void SetExpirationDate(DateTimeOffset dateTimeOffset)
+    {
+        Volatile.Write(ref _expirationUnixTimestamp, dateTimeOffset.ToUnixTimeSeconds());
+    }
+
+    private bool IsTokenExpired()
+    {
+        return Volatile.Read(ref _expirationUnixTimestamp) <= DateTimeOffset.Now.ToUnixTimeSeconds();
     }
 }
