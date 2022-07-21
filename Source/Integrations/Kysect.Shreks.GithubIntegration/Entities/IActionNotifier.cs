662bb55882c61ad8eecc3ce4113eb35a20b8c879
Mikhail Libchenko <libchenkomikhail@gmail.com>
2022-07-21 04:09:27 +0300

    style: redundant `public` keyword removed from interface

--- Source/Integrations/Kysect.Shreks.GithubIntegration/Entities/IActionNotifier.cs
+++ Source/Integrations/Kysect.Shreks.GithubIntegration/Entities/IActionNotifier.cs
@@ -3,6 +3,6 @@ using Octokit.Webhooks;
 namespace Kysect.Shreks.GithubIntegration.Entities;
 
 public interface IActionNotifier
-{
-    public Task ApplyInComments(WebhookEvent webhookEvent, long issueNumber, string hook);
+{ 
+    Task ApplyInComments(WebhookEvent webhookEvent, long issueNumber, string hook);
 }
