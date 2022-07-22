62f7658b229c7fa61258bff632a5f7fa27fc82a2
Mikhail Libchenko <libchenkomikhail@gmail.com>
2022-07-22 15:42:05 +0300

    chore: (temp) GithubIntegration host moved to /Integrations folder

--- Source/Presentations/Kysect.Shreks.GithubIntegration.Host/Program.cs
+++ Source/Presentations/Kysect.Shreks.GithubIntegration.Host/Program.cs
@@ -3,12 +3,7 @@ using Kysect.Shreks.GithubIntegration.Helpers;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var configuration = new ConfigurationBuilder()
-    .AddJsonFile("appsettings.Development.json")
-    .AddEnvironmentVariables()
-    .Build();
-
-var shreksConfiguration = configuration.GetSection(nameof(ShreksConfiguration)).Get<ShreksConfiguration>();
+var shreksConfiguration = builder.Configuration.GetSection(nameof(ShreksConfiguration)).Get<ShreksConfiguration>();
 shreksConfiguration.Verify();
 
 builder.Services
