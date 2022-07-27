7273b3631e948891702c5098d511f5aaf16e2f90
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2022-07-28 00:57:42 +0300

    chore: add sheet enums

---
+++ Source/Integrations/Kysect.Shreks.GoogleIntegration/SheetEnums/UpdatedFields.cs
@@ -0,0 +1,8 @@
+﻿namespace Kysect.Shreks.GoogleIntegration.SheetEnums;
+
+public static class UpdatedFields
+{
+    public const string All = "*";
+    public const string FrozenRowCount = "gridProperties.frozenRowCount";
+    public const string FrozenColumnCount = "gridProperties.frozenColumnCount";
+}
