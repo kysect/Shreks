5c58c6b8e8c5a33041f74189d29aba80941753b4
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2022-07-28 20:42:20 +0300

    refactor: make middleName optional

--- Source/Domain/Kysect.Shreks.Core/Users/User.cs
+++ Source/Domain/Kysect.Shreks.Core/Users/User.cs
@@ -8,11 +8,10 @@ public partial class User : IEntity<Guid>
 {
     private readonly HashSet<UserAssociation> _associations;
 
-    public User(string firstName, string middleName, string lastName)
+    public User(string firstName, string? middleName, string lastName)
         : this(Guid.NewGuid())
     {
         ArgumentNullException.ThrowIfNull(firstName);
-        ArgumentNullException.ThrowIfNull(middleName);
         ArgumentNullException.ThrowIfNull(lastName);
 
         FirstName = firstName;
@@ -23,7 +22,7 @@ public partial class User : IEntity<Guid>
     }
 
     public string FirstName { get; set; }
-    public string MiddleName { get; set; }
+    public string? MiddleName { get; set; }
     public string LastName { get; set; }
 
     public virtual IReadOnlyCollection<UserAssociation> Associations => _associations;
