d54cdf724a4b233ab947a9fee0f00a257e409c39
bibletoon <alex8.ivanov@gmail.com>
2022-07-30 11:23:50 +0300

    Fix: application exceptions inheritance

---
+++ Source/Application/Kysect.Shreks.Application.Abstractions/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,10 @@
+using Kysect.Shreks.Application.Exceptions;
+
+namespace Kysect.Shreks.Application.Abstractions.Exceptions;
+
+public class EntityNotFoundException : ShreksApplicationException
+{ 
+    public EntityNotFoundException(string message) : base(message)
+    {
+    }
+}
--- Source/Application/Kysect.Shreks.Application/Exceptions/EntityNotFoundException.cs
+++
@@ -1,8 +0,0 @@
-namespace Kysect.Shreks.Application.Exceptions;
-
-public class EntityNotFoundException : Exception
-{ 
-    public EntityNotFoundException(string message) : base(message)
-    {
-    }
-}
