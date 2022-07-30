14d31daadab4b8a9e70fc936f6ed44fb91ad7f4d
bibletoon <alex8.ivanov@gmail.com>
2022-07-30 11:25:32 +0300

    Refactor: remove unused exceptions

--- Source/Application/Kysect.Shreks.Application.Abstractions/Exceptions/ShreksApplicationException.cs
+++
@@ -1,8 +0,0 @@
-namespace Kysect.Shreks.Application.Exceptions;
-
-public class ShreksApplicationException : Exception
-{
-    protected ShreksApplicationException() { }
-    protected ShreksApplicationException(string? message) : base(message) { }
-    protected ShreksApplicationException(string? message, Exception? innerException) : base(message, innerException) { }
-}
