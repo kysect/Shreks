4e9a141e91648fac7b5a221810b6bc3c56579017
ronimizy <ronimizy@outlook.com>
2022-07-31 20:09:25 +0300

    feat: added validation for Submission Points

--- Source/Domain/Kysect.Shreks.Core/Study/Submission.cs
+++ Source/Domain/Kysect.Shreks.Core/Study/Submission.cs
@@ -1,3 +1,4 @@
+using Kysect.Shreks.Core.Exceptions;
 using Kysect.Shreks.Core.Users;
 using Kysect.Shreks.Core.ValueObject;
 using RichEntity.Annotations;
@@ -6,19 +7,36 @@ namespace Kysect.Shreks.Core.Study;
 
 public partial class Submission : IEntity<Guid>
 {
+    private Points _points;
+
     public Submission(Student student, Assignment assignment, DateTime submissionDateTime) : this(Guid.NewGuid())
     {
         SubmissionDateTime = submissionDateTime;
-        Points = Points.None;
         Student = student;
         Assignment = assignment;
+        _points = Points.None;
     }
 
     public DateTime SubmissionDateTime { get; set; }
 
-    public Points Points { get; set; }
-
     public virtual Student Student { get; protected init; }
 
     public virtual Assignment Assignment { get; protected init; }
+
+    public Points Points
+    {
+        get => _points;
+        set => SetPoints(value);
+    }
+
+    private void SetPoints(Points points)
+    {
+        if (points < Assignment.MinPoints || Assignment.MaxPoints < points)
+        {
+            var message = $"Cannot rate submission for assignment {Assignment} with points {points}";
+            throw new DomainInvalidOperationException(message);
+        }
+
+        _points = points;
+    }
 }
