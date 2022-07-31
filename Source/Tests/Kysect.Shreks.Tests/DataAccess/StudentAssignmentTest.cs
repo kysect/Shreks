28659d22d66a3be389fa4f91418ecd52e2c045e7
luck-o-o-luck <pks2002@yandex.ru>
2022-07-31 15:48:51 +0300

    add: tests for generators

---
+++ Source/Tests/Kysect.Shreks.Tests/DataAccess/StudentAssignmentTest.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using Kysect.Shreks.Core.Study;
+using Kysect.Shreks.Seeding.EntityGenerators;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Kysect.Shreks.Tests.DataAccess;
+
+public class StudentAssignmentTest : DataAccessTestBase
+{
+    private readonly IEntityGenerator<StudentAssignment> _studentAssignmentGenerator;
+
+    public StudentAssignmentTest()
+    {
+        _studentAssignmentGenerator = Provider.GetRequiredService<IEntityGenerator<StudentAssignment>>();
+    }
+}
