3db6b43046bdf93b4d5ebd3baa04b5e71511931e
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2022-08-11 18:02:59 +0300

    feat: student component factory

---
+++ Source/Infrastructure/Integration/Kysect.Shreks.Integration.Google/Segments/Factories/IStudentComponentFactory.cs
@@ -0,0 +1,11 @@
+﻿using FluentSpreadsheets;
+using Kysect.Shreks.Core.Users;
+
+namespace Kysect.Shreks.Integration.Google.Segments.Factories;
+
+public interface IStudentComponentFactory
+{
+    IComponent BuildHeader();
+
+    public IComponent BuildRow(Student student);
+}
