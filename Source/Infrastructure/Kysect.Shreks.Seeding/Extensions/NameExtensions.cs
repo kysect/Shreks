82548a860be6d18629ef54582f1ac6a565944384
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2022-08-11 23:13:06 +0300

    refactor: inline magic number

--- Source/Infrastructure/Kysect.Shreks.Seeding/Extensions/NameExtensions.cs
+++ Source/Infrastructure/Kysect.Shreks.Seeding/Extensions/NameExtensions.cs
@@ -4,9 +4,11 @@ namespace Kysect.Shreks.Seeding.Extensions;
 
 internal static class NameExtensions
 {
+    private const float ChanceOfHavingMiddleName = 0.95f;
+
     public static string MiddleName(this Name name)
     {
-        return name.Random.Bool(0.95f)
+        return name.Random.Bool(ChanceOfHavingMiddleName)
             ? name.FirstName()
             : string.Empty;
     }
