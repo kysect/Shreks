113cf8f49633719a2e1e5cd7785349e9cbb5849c
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2022-08-11 18:05:01 +0300

    feat: component extensions

---
+++ Source/Infrastructure/Integration/Kysect.Shreks.Integration.Google/Extensions/ComponentExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+using FluentSpreadsheets;
+
+namespace Kysect.Shreks.Integration.Google.Extensions;
+
+public static class ComponentExtensions
+{
+    public static IComponent WithDefaultStyle(this IComponent component)
+    {
+        return component
+            .WithDefaultBorders()
+            .WithCenterAlignment();
+    }
+
+    public static IComponent WithDefaultBorders(this IComponent component)
+    {
+        return component
+            .WithTrailingBorder(BorderType.Thin, Color.Black)
+            .WithBottomBorder(BorderType.Thin, Color.Black);
+    }
+
+    public static IComponent WithCenterAlignment(this IComponent component)
+        => component.WithContentAlignment(HorizontalAlignment.Center, VerticalAlignment.Center);
+}
