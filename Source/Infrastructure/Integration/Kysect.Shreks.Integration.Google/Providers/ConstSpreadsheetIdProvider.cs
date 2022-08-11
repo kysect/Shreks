a2d9676b5390af0f77178fe02e55f3eb88bbc520
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2022-08-11 18:03:32 +0300

    feat: spreadsheet ID provider

---
+++ Source/Infrastructure/Integration/Kysect.Shreks.Integration.Google/Providers/ConstSpreadsheetIdProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace Kysect.Shreks.Integration.Google.Providers;
+
+public class ConstSpreadsheetIdProvider : ISpreadsheetIdProvider
+{
+    public ConstSpreadsheetIdProvider(string spreadSheetId)
+    {
+        SpreadsheetId = spreadSheetId;
+    }
+
+    public string SpreadsheetId { get; }
+}
