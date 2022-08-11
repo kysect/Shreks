2750828cac5c56a56816a69cbee2eca1dd962a09
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2022-08-11 18:03:44 +0300

    feat: culture info provider

---
+++ Source/Infrastructure/Integration/Kysect.Shreks.Integration.Google/Providers/RuCultureInfoProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+
+namespace Kysect.Shreks.Integration.Google.Providers;
+
+public class RuCultureInfoProvider : ICultureInfoProvider
+{
+    private static readonly CultureInfo RuCultureInfo = new("ru-RU");
+
+    public CultureInfo CultureInfo => RuCultureInfo;
+}
