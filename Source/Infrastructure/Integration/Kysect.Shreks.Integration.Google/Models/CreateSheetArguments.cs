9684d8f73a5cde1ccbcf947915e721aeab17996d
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2022-08-11 18:12:20 +0300

    refactor: remove unused files

--- Source/Infrastructure/Integration/Kysect.Shreks.Integration.Google/Models/CreateSheetArguments.cs
+++
@@ -1,9 +0,0 @@
-﻿using Kysect.Shreks.Integration.Google.Tools;
-
-namespace Kysect.Shreks.Integration.Google.Models;
-
-public readonly record struct CreateSheetArguments(
-    int SheetId,
-    SheetRange HeaderSheetRange,
-    SheetRange DataSheetRange,
-    GoogleTableEditor Editor);
--- Source/Infrastructure/Integration/Kysect.Shreks.Integration.Google/SheetEnums/UpdatedFields.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Kysect.Shreks.Integration.Google.SheetEnums;
-
-public static class UpdatedFields
-{
-    public const string All = "*";
-    public const string FrozenRowCount = "gridProperties.frozenRowCount";
-    public const string FrozenColumnCount = "gridProperties.frozenColumnCount";
-}
