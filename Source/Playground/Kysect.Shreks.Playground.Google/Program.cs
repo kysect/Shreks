960794de65981aca54bf74dcb9a66e1fcfba5b36
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2022-08-11 22:35:40 +0300

    feat: playground logic

--- Source/Playground/Kysect.Shreks.Playground.Google/Program.cs
+++ Source/Playground/Kysect.Shreks.Playground.Google/Program.cs
@@ -3,9 +3,13 @@ using Google.Apis.Services;
 using Google.Apis.Sheets.v4;
 using Kysect.Shreks.Application.Abstractions.Google;
 using Kysect.Shreks.Core.Formatters;
+using Kysect.Shreks.Core.Study;
+using Kysect.Shreks.Core.Users;
 using Kysect.Shreks.Integration.Google;
 using Kysect.Shreks.Integration.Google.Extensions;
 using Kysect.Shreks.Integration.Google.Providers;
+using Kysect.Shreks.Seeding.EntityGenerators;
+using Kysect.Shreks.Seeding.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 
 var credential = await GoogleCredential.FromFileAsync("client_secrets.json", default);
@@ -28,6 +32,49 @@ IServiceProvider services = new ServiceCollection()
     .AddSingleton<IGoogleTableAccessor, GoogleTableAccessor>()
     .AddSingleton<IUserFullNameFormatter, UserFullNameFormatter>()
     .AddSingleton<ICultureInfoProvider, RuCultureInfoProvider>()
+    .AddEntityGenerators(o =>
+    {
+        o.ConfigureEntityGenerator<Submission>(s => s.Count = 300); 
+        o.ConfigureEntityGenerator<Student>(s => s.Count = 100); 
+        o.ConfigureEntityGenerator<Assignment>(s => s.Count = 5);
+    })
     .BuildServiceProvider();
 
 var googleTableAccessor = services.GetRequiredService<IGoogleTableAccessor>();
+
+var submissionGenerator = services.GetRequiredService<IEntityGenerator<Submission>>();
+IReadOnlyCollection<Submission> submissions = submissionGenerator.GeneratedEntities;
+
+var queue = new Queue(submissions);
+await googleTableAccessor.UpdateQueueAsync(queue);
+
+var studentGenerator = services.GetRequiredService<IEntityGenerator<Student>>();
+var assignmentGenerator = services.GetRequiredService<IEntityGenerator<Assignment>>();
+
+IReadOnlyCollection<Assignment> assignments = assignmentGenerator.GeneratedEntities;
+
+IReadOnlyCollection<StudentPoints> studentPoints = studentGenerator.GeneratedEntities
+    .Select(s => new StudentPoints(s, GetPoints(s, submissions)))
+    .ToList();
+
+var points = new Points(assignments, studentPoints);
+await googleTableAccessor.UpdatePointsAsync(points);
+
+IReadOnlyCollection<AssignmentPoints> GetPoints(Student student, IReadOnlyCollection<Submission> submissions)
+{
+    return submissions
+        .Where(sub => sub.Student.Equals(student))
+        .GroupBy(sub => sub.Assignment)
+        .Select(sub => sub.MaxBy(g => g.SubmissionDateTime))
+        .Where(sub => sub is not null)
+        .Select(GetAssignmentPoints!)
+        .ToList();
+}
+
+AssignmentPoints GetAssignmentPoints(Submission submission)
+{
+    return new AssignmentPoints(
+        submission.Assignment,
+        DateOnly.FromDateTime(submission.SubmissionDateTime),
+        submission.Points);
+}
