dd4c945c3f70e3e9e01093eb832e855f47469beb
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2022-08-11 18:10:02 +0300

    feat: sheet segments

---
+++ Source/Infrastructure/Integration/Kysect.Shreks.Integration.Google/Segments/AssignmentPointsSegment.cs
@@ -0,0 +1,60 @@
+﻿using System.Globalization;
+using FluentSpreadsheets;
+using FluentSpreadsheets.SheetSegments;
+using Kysect.Shreks.Application.Abstractions.GoogleSheets;
+using Kysect.Shreks.Core.Study;
+using Kysect.Shreks.Integration.Google.Extensions;
+using Kysect.Shreks.Integration.Google.Providers;
+using MediatR;
+using static FluentSpreadsheets.ComponentFactory;
+
+namespace Kysect.Shreks.Integration.Google.Segments;
+
+public class AssignmentPointsSegment : PrototypeSheetSegmentBase<Points, StudentPoints, Unit, Assignment, AssignmentPoints?>
+{
+    private readonly ICultureInfoProvider _cultureInfoProvider;
+
+    public AssignmentPointsSegment(ICultureInfoProvider cultureInfoProvider)
+    {
+        _cultureInfoProvider = cultureInfoProvider;
+    }
+
+    protected override IComponent BuildHeader(Assignment data)
+    {
+        return VStack
+        (
+            Label(data.ShortName).WithDefaultStyle(),
+            HStack
+            (
+                Label("Балл").WithDefaultStyle(),
+                Label("Дата").WithDefaultStyle()
+            )
+        );
+    }
+
+    protected override IComponent BuildRow(HeaderRowData<Assignment, AssignmentPoints?> data, int rowIndex)
+    {
+        CultureInfo currentCulture = _cultureInfoProvider.CultureInfo;
+        AssignmentPoints? assignmentPoints = data.RowData;
+
+        IComponent pointsComponent = assignmentPoints is null
+            ? Empty()
+            : Label(assignmentPoints.Points.ToSheetPoints(currentCulture));
+
+        IComponent dateComponent = assignmentPoints is null
+            ? Empty()
+            : Label(assignmentPoints.Date, currentCulture);
+
+        return HStack
+        (
+            pointsComponent.WithDefaultStyle(),
+            dateComponent.WithDefaultStyle()
+        );
+    }
+
+    protected override IEnumerable<Assignment> SelectHeaderData(Points data)
+        => data.Assignments;
+
+    protected override AssignmentPoints? SelectRowData(HeaderRowData<Assignment, StudentPoints> data)
+        => data.RowData.Points.FirstOrDefault(p => p.Assignment.Equals(data.HeaderData));
+}
