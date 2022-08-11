545a3fe23976367da5bebd7656db7cbbec19d72c
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2022-08-11 18:30:41 +0300

    feat: google table service build

--- Source/Playground/Kysect.Shreks.Playground.Google/Program.cs
+++ Source/Playground/Kysect.Shreks.Playground.Google/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Kysect.Shreks.Application.Abstractions.Google;
+using Kysect.Shreks.Core.Formatters;
+using Kysect.Shreks.Integration.Google;
+using Kysect.Shreks.Integration.Google.Extensions;
+using Kysect.Shreks.Integration.Google.Providers;
+using Microsoft.Extensions.DependencyInjection;
+
+var credential = await GoogleCredential.FromFileAsync("client_secrets.json", default);
+
+var initializer = new BaseClientService.Initializer
+{
+    HttpClientInitializer = credential
+};
+
+var sheetsService = new SheetsService(initializer);
+
+const string spreadSheetId = "";
+
+var spreadsheetIdProvider = new ConstSpreadsheetIdProvider(spreadSheetId);
+
+IServiceProvider services = new ServiceCollection()
+    .AddSheetServices()
+    .AddSingleton(sheetsService)
+    .AddSingleton<ISpreadsheetIdProvider>(spreadsheetIdProvider)
+    .AddSingleton<IGoogleTableAccessor, GoogleTableAccessor>()
+    .AddSingleton<IUserFullNameFormatter, UserFullNameFormatter>()
+    .AddSingleton<ICultureInfoProvider, RuCultureInfoProvider>()
+    .BuildServiceProvider();
+
+var googleTableAccessor = services.GetRequiredService<IGoogleTableAccessor>();
