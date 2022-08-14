2ee3440d28f905ae6037e9d8ccbecc3039444495
bibletoon <alex8.ivanov@gmail.com>
2022-08-14 14:31:05 +0300

    Refactor: fix code smell

--- Source/Application/Kysect.Shreks.Application.Abstractions/Exceptions/NotFoundException.cs
+++ Source/Application/Kysect.Shreks.Application.Abstractions/Exceptions/NotFoundException.cs
@@ -2,7 +2,7 @@ namespace Kysect.Shreks.Application.Abstractions.Exceptions;
 
 public abstract class NotFoundException : ShreksApplicationException
 {
-    protected NotFoundException() : base() { }
+    protected NotFoundException() { }
     protected NotFoundException(string? message) : base(message) { }
     protected NotFoundException(string? message, Exception? innerException) : base(message, innerException) { }
 }
