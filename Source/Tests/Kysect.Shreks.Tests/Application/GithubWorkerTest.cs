49a661099bea2d1fa02ea24532adb763c4f660e7
Fredi Kats <fredikats@outlook.com>
2022-08-14 16:22:06 +0400

    test: draft for GetGithubUsernamesForSubjectCourse test

---
+++ Source/Tests/Kysect.Shreks.Tests/Application/GithubWorkerTest.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Kysect.Shreks.Application.Handlers.Students;
+using Kysect.Shreks.Core.Study;
+using Kysect.Shreks.Tests.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using static Kysect.Shreks.Application.Abstractions.Students.GetGithubUsernamesForSubjectCourse;
+
+namespace Kysect.Shreks.Tests.Application;
+
+public class GithubWorkerTest : DataAccessTestBase
+{
+    //TODO: check after #83 implemented
+    //[Fact]
+    public async Task GetStudentGithubsForSubjectId_NoExceptionThrown()
+    {
+        var handler = new GetGithubUsernamesForSubjectCourseHandler(Context);
+
+        SubjectCourse subjectCourse = await Context.SubjectCourses.FirstAsync();
+
+        Response response = await handler.Handle(new Query(subjectCourse.Id), CancellationToken.None);
+
+        response.StudentGithubUsernames.Should().NotBeNull();
+    }
+
+}
