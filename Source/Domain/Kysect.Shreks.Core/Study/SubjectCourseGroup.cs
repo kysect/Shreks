468c2aafed7421da5264cc378a125aa62043fa0f
ronimizy <ronimizy@outlook.com>
2022-08-14 20:57:19 +0300

    refactor: Mentor SubjectCourse relation

--- Source/Domain/Kysect.Shreks.Core/Study/SubjectCourseGroup.cs
+++ Source/Domain/Kysect.Shreks.Core/Study/SubjectCourseGroup.cs
@@ -1,13 +1,9 @@
-using Kysect.Shreks.Core.Exceptions;
-using Kysect.Shreks.Core.Users;
 using RichEntity.Annotations;
 
 namespace Kysect.Shreks.Core.Study;
 
 public partial class SubjectCourseGroup : IEntity
 {
-    private readonly HashSet<Mentor> _practiceMentors;
-
     public SubjectCourseGroup(SubjectCourse subjectCourse, StudentGroup studentGroup)
         : this(subjectCourseId: subjectCourse.Id, studentGroupId: studentGroup.Id)
     {
@@ -16,7 +12,6 @@ public partial class SubjectCourseGroup : IEntity
 
         SubjectCourse = subjectCourse;
         StudentGroup = studentGroup;
-        _practiceMentors = new HashSet<Mentor>();
     }
 
     [KeyProperty]
@@ -25,24 +20,6 @@ public partial class SubjectCourseGroup : IEntity
     [KeyProperty]
     public virtual StudentGroup StudentGroup { get; protected init; }
 
-    public virtual IReadOnlyCollection<Mentor> PracticeMentors => _practiceMentors;
-
     public override string ToString()
         => StudentGroup.ToString();
-
-    public void AddPracticeMentor(Mentor mentor)
-    {
-        ArgumentNullException.ThrowIfNull(mentor);
-
-        if (!_practiceMentors.Add(mentor))
-            throw new DomainInvalidOperationException($"Mentor {mentor.Id} is already assigned to this group");
-    }
-
-    public void RemovePracticeMentor(Mentor mentor)
-    {
-        ArgumentNullException.ThrowIfNull(mentor);
-
-        if (!_practiceMentors.Remove(mentor))
-            throw new DomainInvalidOperationException($"Mentor {mentor.Id} could not be removed from this group");
-    }
 }
