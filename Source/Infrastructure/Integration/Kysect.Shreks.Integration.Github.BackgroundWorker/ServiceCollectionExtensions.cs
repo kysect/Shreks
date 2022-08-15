545af5054008b707d45f1a168a80798ca3b40b32
Fredi Kats <fredikats@outlook.com>
2022-08-15 15:42:44 +0400

    feat: add method for worker registration in DI

---
+++ Source/Infrastructure/Integration/Kysect.Shreks.Integration.Github.BackgroundWorker/ServiceCollectionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Kysect.Shreks.Integration.Github.BackgroundWorker;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddGithubInviteBackgroundService(this IServiceCollection services)
+    {
+        return services.AddHostedService<GithubInvitingWorker>();
+    }
+}
