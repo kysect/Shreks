338e8223e34c795943ab4a611ef380032479cb83
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2022-08-16 16:02:40 +0300

    feat: course points query

---
+++ Source/Application/Kysect.Shreks.Application.Abstractions/Google/Queries/GetCoursePointsBySubjectCourse.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Kysect.Shreks.Application.Abstractions.Google.Queries;
+
+public static class GetCoursePointsBySubjectCourse
+{
+    public record Query(Guid SubjectCourseId) : IRequest<Response>;
+
+    public record Response(CoursePoints Points);
+}
