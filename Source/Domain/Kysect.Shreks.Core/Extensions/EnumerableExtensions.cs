54db9fb9dc9ce827dce942c17da129c6ddaba888
ronimizy <ronimizy@outlook.com>
2022-08-17 19:17:47 +0300

    fix: async warning

--- Source/Domain/Kysect.Shreks.Core/Extensions/EnumerableExtensions.cs
+++ Source/Domain/Kysect.Shreks.Core/Extensions/EnumerableExtensions.cs
@@ -4,6 +4,8 @@ namespace Kysect.Shreks.Core.Extensions;
 
 internal static class EnumerableExtensions
 {
+    // Async enumerable generator with `yield return` must be an `async` method.
+#pragma warning disable CS1998
     public static async IAsyncEnumerable<T> AsAsyncEnumerable<T>(
         this IEnumerable<T> enumerable,
         [EnumeratorCancellation] CancellationToken cancellationToken = default)
