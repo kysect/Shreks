6b4593f2b03ad7fef4a792d1e8d843156647fded
ronimizy <ronimizy@outlook.com>
2022-08-17 19:42:17 +0300

    refactor: AsAsyncEnumerable method

--- Source/Domain/Kysect.Shreks.Core/Extensions/EnumerableExtensions.cs
+++ Source/Domain/Kysect.Shreks.Core/Extensions/EnumerableExtensions.cs
@@ -2,13 +2,14 @@ using System.Runtime.CompilerServices;
 
 namespace Kysect.Shreks.Core.Extensions;
 
+// Async enumerable generator with `yield return` must be an `async` method.
+#pragma warning disable CS1998
+
 internal static class EnumerableExtensions
 {
-    // Async enumerable generator with `yield return` must be an `async` method.
-#pragma warning disable CS1998
     public static async IAsyncEnumerable<T> AsAsyncEnumerable<T>(
         this IEnumerable<T> enumerable,
-        [EnumeratorCancellation] CancellationToken cancellationToken = default)
+        [EnumeratorCancellation] CancellationToken _ = default)
     {
         foreach (var value in enumerable)
         {
