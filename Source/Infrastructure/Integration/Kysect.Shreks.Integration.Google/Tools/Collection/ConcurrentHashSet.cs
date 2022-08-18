37cc41668e97b057d213839ecbc9fea6238fc762
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2022-08-18 16:14:36 +0300

    feat: concurrent hash set

---
+++ Source/Infrastructure/Integration/Kysect.Shreks.Integration.Google/Tools/Collection/ConcurrentHashSet.cs
@@ -0,0 +1,26 @@
+﻿namespace Kysect.Shreks.Integration.Google.Tools.Collection;
+
+public class ConcurrentHashSet<T>
+{
+    private readonly object _lock = new();
+
+    private readonly HashSet<T> _hastSet = new();
+
+    public void Add(T value)
+    {
+        lock (_lock)
+        {
+            _hastSet.Add(value);
+        }
+    }
+
+    public IReadOnlyCollection<T> GetAndClearValues()
+    {
+        lock (_lock)
+        {
+            var values = _hastSet.ToArray();
+            _hastSet.Clear();
+            return values;
+        }
+    }
+}
