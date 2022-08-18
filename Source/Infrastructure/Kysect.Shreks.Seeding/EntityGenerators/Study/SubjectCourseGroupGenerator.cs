f7bfd3f6ab7aafd7d74f38fbd16e3e13f3564ab5
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2022-08-19 00:09:05 +0300

    fix: update SubjectCourseGroupGenerator

--- Source/Infrastructure/Kysect.Shreks.Seeding/EntityGenerators/Study/SubjectCourseGroupGenerator.cs
+++ Source/Infrastructure/Kysect.Shreks.Seeding/EntityGenerators/Study/SubjectCourseGroupGenerator.cs
@@ -8,30 +8,34 @@ public class SubjectCourseGroupGenerator : EntityGeneratorBase<SubjectCourseGrou
 {
     private readonly IEntityGenerator<SubjectCourse> _subjectCourseGenerator;
     private readonly IEntityGenerator<StudentGroup> _studentGroupGenerator;
-    private readonly Faker _faker;
-    
+
     public SubjectCourseGroupGenerator(
         EntityGeneratorOptions<SubjectCourseGroup> options,
         IEntityGenerator<SubjectCourse> subjectCourseGenerator,
-        IEntityGenerator<StudentGroup> studentGroupGenerator,
-        Faker faker) 
+        IEntityGenerator<StudentGroup> studentGroupGenerator)
         : base(options)
     {
         _subjectCourseGenerator = subjectCourseGenerator;
         _studentGroupGenerator = studentGroupGenerator;
-        _faker = faker;
     }
 
     protected override SubjectCourseGroup Generate(int index)
     {
         var studentGroupCount = _studentGroupGenerator.GeneratedEntities.Count;
         var studentGroupNumber = index % studentGroupCount;
-        
-        var subjectCourse = _faker.PickRandom<SubjectCourse>(_subjectCourseGenerator.GeneratedEntities);
+
+        var subjectCourseCount = _subjectCourseGenerator.GeneratedEntities.Count;
+        var subjectCourseGroupNumber = index / studentGroupCount;
+
+        if (subjectCourseGroupNumber >= subjectCourseCount)
+            throw new IndexOutOfRangeException(
+                "The subject course group index is greater than the number of subject courses.");
+
+        var subjectCourse = _subjectCourseGenerator.GeneratedEntities[subjectCourseGroupNumber];
         var studentGroup = _studentGroupGenerator.GeneratedEntities[studentGroupNumber];
-        
+
         var subjectCourseGroup = new SubjectCourseGroup(subjectCourse, studentGroup);
-        
+
         subjectCourse.AddGroup(subjectCourseGroup);
 
         return subjectCourseGroup;
