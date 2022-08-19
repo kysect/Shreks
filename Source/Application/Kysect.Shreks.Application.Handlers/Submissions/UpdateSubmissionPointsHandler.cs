9d15229a9992a2d865960cae57a11bd28b26e344
Fredi Kats <fredikats@outlook.com>
2022-08-19 09:44:09 +0400

    feat: extra points updating

--- Source/Application/Kysect.Shreks.Application.Handlers/Submissions/UpdateSubmissionPointsHandler.cs
+++ Source/Application/Kysect.Shreks.Application.Handlers/Submissions/UpdateSubmissionPointsHandler.cs
@@ -1,9 +1,11 @@
 using AutoMapper;
 using Kysect.Shreks.Application.Abstractions.DataAccess;
 using Kysect.Shreks.Application.Dto.Study;
+using Kysect.Shreks.Core.Exceptions;
 using Kysect.Shreks.Core.ValueObject;
 using MediatR;
 using static Kysect.Shreks.Application.Abstractions.Submissions.Commands.UpdateSubmissionPoints;
+
 namespace Kysect.Shreks.Application.Handlers.Submissions;
 
 public class UpdateSubmissionPointsHandler : IRequestHandler<Command, Response>
@@ -19,9 +21,17 @@ public class UpdateSubmissionPointsHandler : IRequestHandler<Command, Response>
 
     public async Task<Response> Handle(Command request, CancellationToken cancellationToken)
     {
+        if (request.NewRating is null && request.ExtraPoints is null)
+            throw new DomainInvalidOperationException($"Cannot update submission points, both {request.NewRating} and {request.ExtraPoints} are null.");
+
         var submission = await _context.Submissions.GetByIdAsync(request.SubmissionId, cancellationToken);
 
-        submission.Rating = new Rating(request.NewRating);
+        if (request.NewRating is not null)
+            submission.Rating = new Rating(request.NewRating.Value);
+
+        if (request.ExtraPoints is not null)
+            submission.ExtraPoints = request.ExtraPoints.Value;
+
         _context.Submissions.Update(submission);
         await _context.SaveChangesAsync(cancellationToken);
 
