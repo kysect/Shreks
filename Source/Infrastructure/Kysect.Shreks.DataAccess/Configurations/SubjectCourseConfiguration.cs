a908f5884280683ebc2861f34ec38797fbc11efe
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2022-08-19 19:02:27 +0300

    fix: SubjectCourse configuration

--- Source/Infrastructure/Kysect.Shreks.DataAccess/Configurations/SubjectCourseConfiguration.cs
+++ Source/Infrastructure/Kysect.Shreks.DataAccess/Configurations/SubjectCourseConfiguration.cs
@@ -9,6 +9,7 @@ public class SubjectCourseConfiguration : IEntityTypeConfiguration<SubjectCourse
     public void Configure(EntityTypeBuilder<SubjectCourse> builder)
     {
         builder.HasOne(x => x.Subject);
+        builder.HasOne(x => x.Name);
 
         builder.Navigation(x => x.Groups).HasField("_groups");
         builder.Navigation(x => x.Assignments).HasField("_assignments");
