43cb02c7de89dfa9c9053c05581aea22891914d6
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2022-08-20 23:17:10 +0300

    feat: GetSubmissionsQueueBySubjectCourseHandler

---
+++ Source/Application/Kysect.Shreks.Application.Handlers/Google/GetSubmissionsQueueBySubjectCourseHandler.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Kysect.Shreks.Application.Dto.Study;
+using Kysect.Shreks.Application.Dto.Tables;
+using Kysect.Shreks.Application.Dto.Users;
+using Kysect.Shreks.Core.Study;
+using Kysect.Shreks.DataAccess.Abstractions;
+using MediatR;
+using static Kysect.Shreks.Application.Abstractions.Google.Queries.GetSubmissionsQueueBySubjectCourse;
+
+namespace Kysect.Shreks.Application.Handlers.Google;
+
+public class GetSubmissionsQueueBySubjectCourseHandler : IRequestHandler<Query, Response>
+{
+    private readonly IShreksDatabaseContext _context;
+    private readonly IMapper _mapper;
+
+    public GetSubmissionsQueueBySubjectCourseHandler(IShreksDatabaseContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<Response> Handle(Query request, CancellationToken cancellationToken)
+    {
+        var submissions = new List<Submission>();
+        //TODO: add queue logic
+
+        var queueSubmissionsDto = submissions
+            .Select(s => (
+                Submission: _mapper.Map<SubmissionDto>(s),
+                Student: _mapper.Map<StudentDto>(s.Student)))
+            .Select(t => new QueueSubmissionDto(t.Student, t.Submission))
+            .ToArray();
+
+        var submissionsQueue = new SubmissionsQueueDto(queueSubmissionsDto);
+        return new Response(submissionsQueue);
+    }
+}
