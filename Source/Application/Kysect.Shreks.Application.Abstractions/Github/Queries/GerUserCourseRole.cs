aab768bac04aeb31396627a88307f4aa5b90f616
hrrrrustic <hrrrrustic@gmail.com>
2022-08-20 20:49:42 +0300

    Add user roles query models

---
+++ Source/Application/Kysect.Shreks.Application.Abstractions/Github/Queries/GerUserCourseRole.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace Kysect.Shreks.Application.Abstractions.Github.Queries;
+
+public class GerUserCourseRole
+{
+    public enum UserCourseRole
+    {
+        Unknown = 1,
+        Student = 2,
+        Mentor = 3
+    }
+
+    public record Query(Guid SubjectCourseId, Guid UserId) : IRequest<Response>;
+    public record Response(UserCourseRole Role);    
+}
