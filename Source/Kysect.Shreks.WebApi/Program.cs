ccbc388d3f251a8db4215c03171167d077d61d8f
Fredi Kats <fredikats@outlook.com>
2022-08-22 01:50:14 +0400

    feat: implement logic for debug launching with user github associations

--- Source/Kysect.Shreks.WebApi/Program.cs
+++ Source/Kysect.Shreks.WebApi/Program.cs
@@ -1,10 +1,16 @@
 using Kysect.Shreks.Application.Commands.Extensions;
 using Kysect.Shreks.Application.Handlers.Extensions;
+using Kysect.Shreks.Core.Study;
+using Kysect.Shreks.Core.SubjectCourseAssociations;
+using Kysect.Shreks.Core.UserAssociations;
+using Kysect.Shreks.Core.Users;
+using Kysect.Shreks.DataAccess.Abstractions;
 using Kysect.Shreks.DataAccess.Extensions;
 using Kysect.Shreks.Integration.Github.Extensions;
 using Kysect.Shreks.Integration.Github.Helpers;
 using Kysect.Shreks.Integration.Google.Extensions;
 using Kysect.Shreks.Mapping.Extensions;
+using Kysect.Shreks.Seeding.EntityGenerators;
 using Kysect.Shreks.Seeding.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
@@ -15,6 +21,7 @@ builder.Host.UseSerilog(
     (ctx, lc) => lc.MinimumLevel.Verbose().WriteTo.Console());
 
 ShreksConfiguration shreksConfiguration = builder.Configuration.GetShreksConfiguration();
+TestEnvConfiguration testEnvConfiguration = builder.Configuration.GetSection(nameof(TestEnvConfiguration)).Get<TestEnvConfiguration>();
 
 InitServiceCollection(builder);
 await InitWebApplication(builder);
@@ -62,14 +69,46 @@ async Task InitWebApplication(WebApplicationBuilder webApplicationBuilder)
         app.UseCors(opt => opt.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
     }
 
-    app.UseHttpsRedirection();
+    //app.UseHttpsRedirection();
 
-    app.UseAuthorization();
+    //app.UseAuthorization();
 
     app.MapControllers();
     app.UseSerilogRequestLogging();
 
     app.UseGithubIntegration(shreksConfiguration);
+    await InitTestEnvironment(app.Services, testEnvConfiguration);
 
     app.Run();
+}
+
+async Task InitTestEnvironment(
+    IServiceProvider serviceProvider,
+    TestEnvConfiguration config,
+    CancellationToken cancellationToken = default)
+{
+    using var scope = serviceProvider.CreateScope();
+
+    var dbContext = scope.ServiceProvider.GetRequiredService<IShreksDatabaseContext>();
+
+    var userGenerator = serviceProvider.GetRequiredService<IEntityGenerator<User>>();
+    var users = userGenerator.GeneratedEntities;
+    dbContext.Users.AttachRange(users);
+
+    User first = users.First();
+    var login = config.Users[0];
+    dbContext.UserAssociations.Add(new GithubUserAssociation(first, login));
+
+    var subjectCourseGenerator = serviceProvider.GetRequiredService<IEntityGenerator<SubjectCourse>>();
+    var subjectCourse = subjectCourseGenerator.GeneratedEntities[0];
+    dbContext.SubjectCourses.Attach(subjectCourse);
+    dbContext.SubjectCourseAssociations.Add(new GithubSubjectCourseAssociation(subjectCourse, config.Organization));
+
+    await dbContext.SaveChangesAsync(cancellationToken);
+}
+
+public class TestEnvConfiguration
+{
+    public string Organization { get; init; }
+    public List<string> Users { get; init; }
 }
