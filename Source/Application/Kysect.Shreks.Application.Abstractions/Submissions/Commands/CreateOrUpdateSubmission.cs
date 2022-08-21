26ca4413ef13d958d4afdb0a8c322969b595f459
Vladislav Grigoriev <vvllaadd99@yandex.ru>
2022-08-21 18:35:17 +0300

    implement processing synchronized and opened action

--- Source/Application/Kysect.Shreks.Application.Abstractions/Submissions/Commands/CreateOrUpdateSubmission.cs
+++ Source/Application/Kysect.Shreks.Application.Abstractions/Submissions/Commands/CreateOrUpdateSubmission.cs
@@ -5,7 +5,7 @@ namespace Kysect.Shreks.Application.Abstractions.Submissions.Commands;
 
 public static class CreateOrUpdateSubmissionCommand
 {
-    public record Command(Guid StudentId, Guid AssignmentId, string Payload, string Organization, string Repository, int PrNumber) : IRequest<Response>;
+    public record Command(Guid StudentId, Guid AssignmentId, string Payload, string Organization, string Repository, long PrNumber) : IRequest<Response>;
 
     public record Response(SubmissionDto Submission);
 }
