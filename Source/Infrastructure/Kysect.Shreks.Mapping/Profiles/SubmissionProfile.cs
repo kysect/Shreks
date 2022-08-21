1a667744b74ab59ce1e4bb70fc7b5230725ed508
Fredi Kats <fredikats@outlook.com>
2022-08-21 12:57:48 +0400

    feat: link submissions to group assignment

---
+++ Source/Infrastructure/Kysect.Shreks.Mapping/Profiles/SubmissionProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Kysect.Shreks.Application.Dto.Study;
+using Kysect.Shreks.Core.Study;
+
+namespace Kysect.Shreks.Mapping.Profiles;
+
+public class SubmissionProfile : Profile
+{
+    public SubmissionProfile()
+    {
+        CreateMap<Submission, SubmissionDto>()
+            .ConstructUsing(s => new SubmissionDto(
+                s.Id,
+                s.SubmissionDate,
+                s.Student.Id,
+                s.GroupAssignment.AssignmentId,
+                s.Payload,
+                s.ExtraPoints == null ? 0 : s.ExtraPoints.Value.Value,
+                s.Points == null ? 0 : s.Points.Value.Value,
+                s.GroupAssignment.Assignment.ShortName));
+            //.ForMember(dto => dto.AssignmentId,
+            //    opt => opt.MapFrom(src => src.GroupAssignment.AssignmentId))
+            //.ForMember(dto => dto.AssignmentShortName,
+            //    opt => opt.MapFrom(src => src.GroupAssignment.Assignment.ShortName));
+    }
+}
