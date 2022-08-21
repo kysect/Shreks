e5923023e0ae1d680d4732fa7ddd71578104c553
annchous <annchooousss@gmail.com>
2022-08-21 14:00:04 +0400

    feat: add CreateOrUpdate command for Submission

---
+++ Source/Application/Kysect.Shreks.Application.Abstractions/Submissions/Commands/CreateOrUpdateSubmission.cs
@@ -0,0 +1,11 @@
+﻿using Kysect.Shreks.Application.Dto.Study;
+using MediatR;
+
+namespace Kysect.Shreks.Application.Abstractions.Submissions.Commands;
+
+public static class CreateOrUpdateSubmissionCommand
+{
+    public record Command(Guid StudentId, Guid AssignmentId, string Payload, String Organization, String Repository, Int32 PrNumber) : IRequest<Response>;
+
+    public record Response(SubmissionDto Submission);
+}
