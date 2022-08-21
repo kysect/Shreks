26a6f36f9f6b38e816639ac8b786c55c2910f99e
Fredi Kats <fredikats@outlook.com>
2022-08-21 13:24:12 +0400

    fix: mapping for nullable VO

--- Source/Infrastructure/Kysect.Shreks.Mapping/Profiles/SubmissionProfile.cs
+++ Source/Infrastructure/Kysect.Shreks.Mapping/Profiles/SubmissionProfile.cs
@@ -9,18 +9,11 @@ public class SubmissionProfile : Profile
     public SubmissionProfile()
     {
         CreateMap<Submission, SubmissionDto>()
-            .ConstructUsing(s => new SubmissionDto(
-                s.Id,
-                s.SubmissionDate,
-                s.Student.Id,
-                s.GroupAssignment.AssignmentId,
-                s.Payload,
-                s.ExtraPoints == null ? 0 : s.ExtraPoints.Value.Value,
-                s.Points == null ? 0 : s.Points.Value.Value,
-                s.GroupAssignment.Assignment.ShortName));
-            //.ForMember(dto => dto.AssignmentId,
-            //    opt => opt.MapFrom(src => src.GroupAssignment.AssignmentId))
-            //.ForMember(dto => dto.AssignmentShortName,
-            //    opt => opt.MapFrom(src => src.GroupAssignment.Assignment.ShortName));
+            .ForCtorParam(
+                nameof(SubmissionDto.AssignmentId),
+                opt => opt.MapFrom(src => src.GroupAssignment.AssignmentId))
+            .ForCtorParam(
+                nameof(SubmissionDto.AssignmentShortName),
+                opt => opt.MapFrom(src => src.GroupAssignment.Assignment.ShortName));
     }
 }
