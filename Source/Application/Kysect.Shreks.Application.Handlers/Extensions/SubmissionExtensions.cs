2e9e5c046fa79cb163b9f41fac51c848ae1e4833
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2022-08-22 15:13:13 +0300

    feat: update GetSubmissionsQueue

--- Source/Application/Kysect.Shreks.Application.Handlers/Extensions/SubmissionExtensions.cs
+++ Source/Application/Kysect.Shreks.Application.Handlers/Extensions/SubmissionExtensions.cs
@@ -6,4 +6,7 @@ internal static class SubmissionExtensions
 {
     public static Guid GetCourseId(this Submission submission)
         => submission.GroupAssignment.Assignment.SubjectCourse.Id;
+
+    public static Guid GetGroupId(this Submission submission)
+        => submission.GroupAssignment.GroupId;
 }
--- Source/Application/Kysect.Shreks.Application.Handlers/Google/GetSubmissionsQueueByCourseAndGroupHandler.cs
+++ Source/Application/Kysect.Shreks.Application.Handlers/Google/GetSubmissionsQueueByCourseAndGroupHandler.cs
@@ -2,19 +2,20 @@
 using Kysect.Shreks.Application.Dto.Study;
 using Kysect.Shreks.Application.Dto.Tables;
 using Kysect.Shreks.Application.Dto.Users;
+using Kysect.Shreks.Core.Study;
 using Kysect.Shreks.DataAccess.Abstractions;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using static Kysect.Shreks.Application.Abstractions.Google.Queries.GetSubmissionsQueueBySubjectCourse;
+using static Kysect.Shreks.Application.Abstractions.Google.Queries.GetSubmissionsQueueByCourseAndGroup;
 
 namespace Kysect.Shreks.Application.Handlers.Google;
 
-public class GetSubmissionsQueueBySubjectCourseHandler : IRequestHandler<Query, Response>
+public class GetSubmissionsQueueByCourseAndGroupHandler : IRequestHandler<Query, Response>
 {
     private readonly IShreksDatabaseContext _context;
     private readonly IMapper _mapper;
 
-    public GetSubmissionsQueueBySubjectCourseHandler(IShreksDatabaseContext context, IMapper mapper)
+    public GetSubmissionsQueueByCourseAndGroupHandler(IShreksDatabaseContext context, IMapper mapper)
     {
         _context = context;
         _mapper = mapper;
@@ -22,10 +23,15 @@ public class GetSubmissionsQueueBySubjectCourseHandler : IRequestHandler<Query, 
 
     public async Task<Response> Handle(Query request, CancellationToken cancellationToken)
     {
-        var submissions = await _context.Submissions
-            .Where(s => s.GroupAssignment.Assignment.SubjectCourse.Id == request.SubjectCourseId)
-            .ToArrayAsync(cancellationToken);
+        var (courseId, groupId) = request;
 
+        var groupName = _context.StudentGroups.First(g => g.Id == groupId).Name;
+        
+        var submissions = await _context.Submissions
+            .Where(s => s.Student.Group.Id == groupId
+                        && s.GroupAssignment.Assignment.SubjectCourse.Id == courseId)
+            .ToArrayAsync(cancellationToken);
+        
         //TODO: add queue logic
 
         var queueSubmissionsDto = submissions
@@ -34,8 +40,8 @@ public class GetSubmissionsQueueBySubjectCourseHandler : IRequestHandler<Query, 
                 Student: _mapper.Map<StudentDto>(s.Student)))
             .Select(t => new QueueSubmissionDto(t.Student, t.Submission))
             .ToArray();
-
-        var submissionsQueue = new SubmissionsQueueDto(queueSubmissionsDto);
+        
+        var submissionsQueue = new SubmissionsQueueDto(groupName, queueSubmissionsDto);
         return new Response(submissionsQueue);
     }
 }
