cac82ca2771073bc48cfaa141aa8f95d657e9492
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2022-08-22 21:39:21 +0300

    docs: update ISheetManagementService docs

--- Source/Infrastructure/Integration/Kysect.Shreks.Integration.Google/Tools/ISheetManagementService.cs
+++ Source/Infrastructure/Integration/Kysect.Shreks.Integration.Google/Tools/ISheetManagementService.cs
@@ -3,11 +3,16 @@
 public interface ISheetManagementService
 {
     /// <summary>
-    /// Clears all values and formatting of a sheet
-    /// if it exists and creates it otherwise
+    ///     Clears all values and formatting of a sheet
+    ///     if it exists and creates it otherwise
     /// </summary>
     /// <returns>Sheet id</returns>
     Task<int> CreateOrClearSheetAsync(string spreadsheetId, string sheetTitle, CancellationToken token);
 
+    /// <summary>
+    ///     Creates spreadsheet in drive,
+    ///     configured by DriveParentProvider
+    /// </summary>
+    /// <returns>Spreadsheet id</returns>
     Task<string> CreateSpreadsheetAsync(string title, CancellationToken token);
 }
