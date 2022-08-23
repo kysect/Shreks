2a16f3349264e7f3f638a0d5dbdcb87fb1ff8035
Fredi Kats <fredikats@outlook.com>
2022-08-24 00:14:05 +0400

    feat: add resolving for pr from issue comment

--- Source/Infrastructure/Integration/Kysect.Shreks.Integration.Github/Processors/ShreksWebhookEventProcessor.cs
+++ Source/Infrastructure/Integration/Kysect.Shreks.Integration.Github/Processors/ShreksWebhookEventProcessor.cs
@@ -9,12 +9,14 @@ using Kysect.Shreks.Integration.Github.ContextFactory;
 using Kysect.Shreks.Integration.Github.Entities;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using Octokit;
 using Octokit.Webhooks;
 using Octokit.Webhooks.Events;
 using Octokit.Webhooks.Events.IssueComment;
 using Octokit.Webhooks.Events.PullRequest;
 using Octokit.Webhooks.Events.PullRequestReview;
 using Octokit.Webhooks.Models;
+using PullRequestReviewEvent = Octokit.Webhooks.Events.PullRequestReviewEvent;
 
 namespace Kysect.Shreks.Integration.Github.Processors;
 
@@ -24,20 +26,21 @@ public sealed class ShreksWebhookEventProcessor : WebhookEventProcessor
     private readonly ILogger<ShreksWebhookEventProcessor> _logger;
     private readonly IShreksCommandParser _commandParser;
     private readonly IMediator _mediator;
-    private readonly IInstallationClientFactory _installationClientFactory;
+    private readonly IOrganizationGithubClientProvider _clientProvider;
+
 
     public ShreksWebhookEventProcessor(
         IActionNotifier actionNotifier, 
         ILogger<ShreksWebhookEventProcessor> logger,
         IShreksCommandParser commandParser, 
         IMediator mediator,
-        IInstallationClientFactory installationClientFactory)
+        IOrganizationGithubClientProvider clientProvider)
     {
         _actionNotifier = actionNotifier;
         _logger = logger;
         _commandParser = commandParser;
         _mediator = mediator;
-        _installationClientFactory = installationClientFactory;
+        _clientProvider = clientProvider;
     }
 
     protected override async Task ProcessPullRequestWebhookAsync(WebhookHeaders headers,
@@ -57,27 +60,26 @@ public sealed class ShreksWebhookEventProcessor : WebhookEventProcessor
             case PullRequestActionValue.Opened:
 
                 CancellationToken cancellationToken = CancellationToken.None;
-                string organization = pullRequestEvent.Organization!.Login;
-                var subjectCourseId = await GetSubjectCourseByOrganization(organization, cancellationToken);
-                
-                var login = pullRequestEvent.Sender!.Login;
-                var userId = await GetUserByGithubLogin(login, cancellationToken);
-                var studentId = await GetStudentByUser(userId, cancellationToken);
 
-                string branch = pullRequestEvent.PullRequest.Head.Ref;
-                var assignmentId =
-                    await GetAssignemntByBranchAndSubjectCourse(branch, subjectCourseId, cancellationToken);
-                
-                long prNum = pullRequestEvent.PullRequest.Number;
-                string repository = pullRequestEvent.Repository!.Name;
+                string login = pullRequestEvent.Sender!.Login;
                 string payload = pullRequestEvent.PullRequest.DiffUrl;
+                string organization = pullRequestEvent.Organization!.Login;
+                string repository = pullRequestEvent.Repository!.Name;
+                string branch = pullRequestEvent.PullRequest.Head.Ref;
+                long prNum = pullRequestEvent.PullRequest.Number;
 
-                GithubPullRequestDescriptor pullRequestDescriptor = new GithubPullRequestDescriptor(
+                var pullRequestDescriptor = new GithubPullRequestDescriptor(
+                    login,
                     payload,
                     organization,
                     repository,
                     branch,
                     prNum);
+
+                var subjectCourseId = await GetSubjectCourseByOrganization(organization, cancellationToken);
+                var userId = await GetUserByGithubLogin(login, cancellationToken);
+                var studentId = await GetStudentByUser(userId, cancellationToken);
+                var assignmentId = await GetAssignemntByBranchAndSubjectCourse(branch, subjectCourseId, cancellationToken);
 
                 var command = new CreateOrUpdateGithubSubmission.Command(studentId, assignmentId, pullRequestDescriptor);
 
@@ -126,6 +128,14 @@ public sealed class ShreksWebhookEventProcessor : WebhookEventProcessor
             return;
         }
 
+        if (IsPullRequestCommand(issueCommentEvent))
+        {
+            _logger.LogTrace($"Skip commit in {issueCommentEvent.Issue.Id}. Issue comments is not supported.");
+            return;
+        }
+
+        GithubPullRequestDescriptor pullRequestDescriptor = await GetPullRequestDescriptor(issueCommentEvent);
+
         switch (action)
         {
             case IssueCommentActionValue.Edited:
@@ -137,7 +147,7 @@ public sealed class ShreksWebhookEventProcessor : WebhookEventProcessor
                     if (comment.FirstOrDefault() == '/')
                     {
                         IShreksCommand command = _commandParser.Parse(comment);
-                        var contextCreator = new IssueCommentContextFactory(_mediator, issueCommentEvent, _installationClientFactory);
+                        var contextCreator = new PullRequestCommentContextFactory(_mediator, pullRequestDescriptor, _clientProvider);
                         var processor = new GithubCommandProcessor(contextCreator, CancellationToken.None);
                         var result = await command.AcceptAsync(processor);
                         if (string.IsNullOrEmpty(result.Message))
@@ -165,6 +175,29 @@ public sealed class ShreksWebhookEventProcessor : WebhookEventProcessor
             case IssueCommentActionValue.Deleted:
                 break;
         }
+    }
+
+    private bool IsPullRequestCommand(IssueCommentEvent issueCommentEvent)
+    {
+        return issueCommentEvent.Issue.PullRequest.Url is not null;
+    }
+
+    private async Task<GithubPullRequestDescriptor> GetPullRequestDescriptor(IssueCommentEvent issueCommentEvent)
+    {
+        ArgumentNullException.ThrowIfNull(issueCommentEvent.Sender);
+        ArgumentNullException.ThrowIfNull(issueCommentEvent.Organization);
+        ArgumentNullException.ThrowIfNull(issueCommentEvent.Repository);
+
+        GitHubClient gitHubClient = await _clientProvider.GetClient(issueCommentEvent.Organization.Login);
+        PullRequest pullRequest = await gitHubClient.PullRequest.Get(issueCommentEvent.Repository.Id, (int)issueCommentEvent.Issue.Id);
+
+        return new GithubPullRequestDescriptor(
+            issueCommentEvent.Sender.Login,
+            Payload: pullRequest.DiffUrl,
+            issueCommentEvent.Organization.Login,
+            issueCommentEvent.Repository.Name,
+            BranchName: pullRequest.Head.Ref,
+            pullRequest.Number);
     }
 
     private async Task<Guid> GetSubjectCourseByOrganization(string organization, CancellationToken cancellationToken)
