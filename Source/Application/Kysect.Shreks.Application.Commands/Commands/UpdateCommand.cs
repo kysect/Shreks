13f598c5171ecc482be2faddd9f5a21f950c2c4c
Vladislav Grigoriev <vvllaadd99@yandex.ru>
2022-08-24 01:34:22 +0300

    feat: add date update

--- Source/Application/Kysect.Shreks.Application.Commands/Commands/UpdateCommand.cs
+++ Source/Application/Kysect.Shreks.Application.Commands/Commands/UpdateCommand.cs
@@ -10,21 +10,25 @@ namespace Kysect.Shreks.Application.Commands.Commands;
 [Verb("/update")]
 public class UpdateCommand : IShreksCommand<BaseContext, SubmissionDto>
 {
-    public UpdateCommand(string submissionId, double? ratingPercent, double? extraPoints)
+    public UpdateCommand(string submissionId, double? ratingPercent, double? extraPoints, string? dateStr)
     {
         SubmissionId = submissionId;
         RatingPercent = ratingPercent;
         ExtraPoints = extraPoints;
+        DateStr = dateStr;
     }
 
     [Value(0, Required = true, MetaName = "SubmissionId")]
     public string SubmissionId { get; }
     
-    [Option(shortName:'r', longName: "rating", Required = false)]
+    [Option(shortName:'r', longName: "rating", Group = "update",  Required = false)]
     public double? RatingPercent { get; }
     
-    [Option(shortName:'e', longName:"extra", Required = false)]
+    [Option(shortName:'e', longName:"extra", Group = "update", Required = false)]
     public double? ExtraPoints { get; }
+    
+    [Option(shortName:'d', longName:"date", Group = "update", Required = false)]
+    public string? DateStr { get; }
     
     public Task<TResult> AcceptAsync<TResult>(IShreksCommandVisitor<TResult> visitor) 
         where TResult : IShreksCommandResult
@@ -35,9 +39,23 @@ public class UpdateCommand : IShreksCommand<BaseContext, SubmissionDto>
     public async Task<SubmissionDto> ExecuteAsync(BaseContext context, CancellationToken cancellationToken)
     {
         Guid submissionId = Guid.Parse(SubmissionId);
-        var command = new UpdateSubmissionPoints.Command(submissionId, RatingPercent, ExtraPoints);
-        var response = await context.Mediator.Send(command, cancellationToken);
 
-        return response.Submission;
+        SubmissionDto submissionDto = null!;
+        if (RatingPercent is not null || ExtraPoints is not null)
+        {
+            var command = new UpdateSubmissionPoints.Command(submissionId, RatingPercent, ExtraPoints);
+            var response = await context.Mediator.Send(command, cancellationToken);
+            submissionDto = response.Submission;
+        }
+
+        if (DateStr != null)
+        {
+            var date = DateOnly.Parse(DateStr);
+            var command = new UpdateSubmissionDate.Command(submissionId, date);
+            var response = await context.Mediator.Send(command, cancellationToken);
+            submissionDto = response.Submission;
+        }
+
+        return submissionDto;
     }
 }
