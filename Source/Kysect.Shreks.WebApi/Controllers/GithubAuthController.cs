9251915242a7a199c3b4329740e40df4a04c8600
annchous <annchooousss@gmail.com>
2022-08-24 22:27:12 +0400

    feat: add sign in method in github auth controller

---
+++ Source/Kysect.Shreks.WebApi/Controllers/GithubAuthController.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Claims;
+using MediatR;
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kysect.Shreks.WebApi.Controllers;
+
+[ApiController]
+[Route("api/auth/github")]
+public class GithubAuthController : Controller
+{
+    [HttpGet("sign-in")]
+    public IActionResult SignInGithubUser()
+    {
+        return Challenge(new AuthenticationProperties() { RedirectUri = "/" }, "GitHub");
+    }
+}
