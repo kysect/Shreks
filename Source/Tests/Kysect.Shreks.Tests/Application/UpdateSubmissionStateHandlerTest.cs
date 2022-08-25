23b86724dec13cde6e002e600b0941e531f6f6c8
ronimizy <ronimizy@outlook.com>
2022-08-25 10:19:29 +0300

    refactor: handler test

--- Source/Tests/Kysect.Shreks.Tests/Application/UpdateSubmissionStateHandlerTest.cs
+++ Source/Tests/Kysect.Shreks.Tests/Application/UpdateSubmissionStateHandlerTest.cs
@@ -2,6 +2,7 @@ using FluentAssertions;
 using Kysect.Shreks.Application.Abstractions.Submissions.Commands;
 using Kysect.Shreks.Application.Dto.Study;
 using Kysect.Shreks.Application.Handlers.Submissions;
+using Kysect.Shreks.Core.Models;
 using Microsoft.EntityFrameworkCore;
 using Xunit;
 
@@ -10,13 +11,14 @@ namespace Kysect.Shreks.Tests.Application;
 public class UpdateSubmissionStateHandlerTest : ApplicationTestBase
 {
     [Fact]
-    public async Task Handle_ShouldUpdateState()
+    public async Task Handle_Should_UpdateSubmissionState()
     {
         // Arrange
+        const SubmissionStateDto stateDto = SubmissionStateDto.Active;
+        const SubmissionState state = SubmissionState.Active;
         var submission = await Context.Submissions.FirstAsync();
-        var state = SubmissionStateDto.Active;
 
-        var command = new UpdateSubmissionState.Command(submission.Student.User.Id, submission.Id, state);
+        var command = new UpdateSubmissionState.Command(submission.Student.User.Id, submission.Id, stateDto);
 
         var handler = new UpdateSubmissionStateHandler(Context, Mapper);
 
@@ -24,6 +26,7 @@ public class UpdateSubmissionStateHandlerTest : ApplicationTestBase
         var response = await handler.Handle(command, CancellationToken.None);
         
         // Assert
-        response.Submission.State.Should().Be(state);
+        response.Submission.State.Should().Be(stateDto);
+        submission.State.Should().Be(state);
     }
 }
