4a1a1df8f4a329fb990ec904f0ce852f8ac14fdf
Fredi Kats <fredikats@outlook.com>
2022-08-27 23:33:15 +0300

    Update Source/Domain/Kysect.Shreks.Core/DeadlinePolicies/DeadlinePolicy.cs
    Co-authored-by: Annchous <57408376+annchous@users.noreply.github.com>

--- Source/Domain/Kysect.Shreks.Core/DeadlinePolicies/DeadlinePolicy.cs
+++ Source/Domain/Kysect.Shreks.Core/DeadlinePolicies/DeadlinePolicy.cs
@@ -24,7 +24,6 @@ public abstract class DeadlinePolicy : IEquatable<DeadlinePolicy>
     public override int GetHashCode()
         => SpanBeforeActivation.GetHashCode();
 
-
     public override string ToString()
     {
         return $"{GetType()} with span {SpanBeforeActivation}";
