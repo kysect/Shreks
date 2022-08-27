881bf00efae9c7fed570d650c5e7619a2c267f2d
annchous <annchooousss@gmail.com>
2022-08-27 19:43:21 +0400

    feat: add specification for finding active DeadlinePolicy

---
+++ Source/Application/Kysect.Shreks.Application/FindDeadlinePolicySpecification.cs
@@ -0,0 +1,22 @@
+﻿using Kysect.Shreks.Core.DeadlinePolicies;
+using Kysect.Shreks.Core.Submissions;
+
+namespace Kysect.Shreks.Application
+{
+    public static class FindDeadlinePolicySpecification
+    {
+        public static DeadlinePolicy? GetActiveDeadlinePolicy(this Submission submission, DateOnly deadline)
+        {
+            if (submission.SubmissionDate <= deadline)
+                return null;
+
+            var submissionDeadlineOffset = TimeSpan.FromDays(submission.SubmissionDate.DayNumber - deadline.DayNumber);
+            return submission
+                .GroupAssignment
+                .Assignment
+                .DeadlinePolicies
+                .Where(dp => dp.SpanBeforeActivation < submissionDeadlineOffset)
+                .MaxBy(dp => dp.SpanBeforeActivation);
+        }
+    }
+}
