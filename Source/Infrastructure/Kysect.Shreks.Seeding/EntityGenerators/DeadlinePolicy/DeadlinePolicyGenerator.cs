720c7191db56af45c487090ba3d4e9b9fc6713ef
Fredi Kats <fredikats@outlook.com>
2022-08-28 13:52:41 +0400

    fix: remove time span limits in generator

--- Source/Infrastructure/Kysect.Shreks.Seeding/EntityGenerators/DeadlinePolicy/DeadlinePolicyGenerator.cs
+++ Source/Infrastructure/Kysect.Shreks.Seeding/EntityGenerators/DeadlinePolicy/DeadlinePolicyGenerator.cs
@@ -18,10 +18,9 @@ public class DeadlinePolicyGenerator : EntityGeneratorBase<DeadlinePolicy>
     {
         return (index % 3) switch
         {
-            // TODO: remove limit after WI-229
-            0 => new AbsoluteDeadlinePolicy(_faker.Date.Timespan(TimeSpan.FromHours(12)), _faker.Random.Points(0, 10)),
-            1 => new FractionDeadlinePolicy(_faker.Date.Timespan(TimeSpan.FromHours(12)), _faker.Random.Double()),
-            2 => new CappingDeadlinePolicy(_faker.Date.Timespan(TimeSpan.FromHours(12)), _faker.Random.Double(0, 5)),
+            0 => new AbsoluteDeadlinePolicy(_faker.Date.Timespan(), _faker.Random.Points(0, 10)),
+            1 => new FractionDeadlinePolicy(_faker.Date.Timespan(), _faker.Random.Double()),
+            2 => new CappingDeadlinePolicy(_faker.Date.Timespan(), _faker.Random.Double(0, 5)),
             _ => throw new ArgumentOutOfRangeException()
         };
     }
