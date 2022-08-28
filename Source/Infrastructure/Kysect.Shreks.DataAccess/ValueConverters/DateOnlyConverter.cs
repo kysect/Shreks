efa6e97a58780fcf3def74a10124882ffc21d12f
ronimizy <ronimizy@outlook.com>
2022-08-28 12:55:53 +0300

    refactor: convert date only to date time

--- Source/Infrastructure/Kysect.Shreks.DataAccess/ValueConverters/DateOnlyConverter.cs
+++ Source/Infrastructure/Kysect.Shreks.DataAccess/ValueConverters/DateOnlyConverter.cs
@@ -2,9 +2,9 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Kysect.Shreks.DataAccess.ValueConverters;
 
-public class DateOnlyConverter : ValueConverter<DateOnly, long>
+public class DateOnlyConverter : ValueConverter<DateOnly, DateTime>
 {
     public DateOnlyConverter() : base(
-        x => x.ToDateTime(new TimeOnly(0)).Ticks,
-        x => DateOnly.FromDateTime(new DateTime(x))) { }
+        x => x.ToDateTime(new TimeOnly(0)),
+        x => DateOnly.FromDateTime(x)) { }
 }
