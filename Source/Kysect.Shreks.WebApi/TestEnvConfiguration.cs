e17e77b570f985d752cd8a4259162837569600c6
Fredi Kats <fredikats@outlook.com>
2022-08-30 21:10:05 +0400

    feat: add template name into github subject course association

--- Source/Kysect.Shreks.WebApi/TestEnvConfiguration.cs
+++ Source/Kysect.Shreks.WebApi/TestEnvConfiguration.cs
@@ -3,6 +3,7 @@ namespace Kysect.Shreks.WebApi;
 public class TestEnvConfiguration
 {
     public string Organization { get; init; }
+    public string TemplateRepository { get; init; }
     public List<string> Users { get; init; }
     public bool UseDummyGithubImplementation { get; init; }
 }
--- Source/Playground/Kysect.Shreks.Playground.Github/TestEnv/TestEnvConfiguration.cs
+++ Source/Playground/Kysect.Shreks.Playground.Github/TestEnv/TestEnvConfiguration.cs
@@ -3,5 +3,6 @@ namespace Kysect.Shreks.Playground.Github.TestEnv;
 public class TestEnvConfiguration
 {
     public string Organization { get; init; }
+    public string TemplateRepository { get; init; }
     public List<string> Users { get; init; }
 }
