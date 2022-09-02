eecc8d44f821a7f7026988ecd7d7a85f6e417ec5
ronimizy <ronimizy@outlook.com>
2022-09-02 21:14:33 +0300

    chore: added missing authorize attributes

--- Source/Kysect.Shreks.WebApi/Controllers/GithubManagementController.cs
+++ Source/Kysect.Shreks.WebApi/Controllers/GithubManagementController.cs
@@ -1,11 +1,14 @@
 ﻿using Kysect.Shreks.Application.Abstractions.Github.Commands;
+using Kysect.Shreks.Identity.Entities;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Kysect.Shreks.WebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(Roles = ShreksIdentityRole.AdminRoleName)]
     public class GithubManagementController : ControllerBase
     {
         private readonly IMediator _mediator;
