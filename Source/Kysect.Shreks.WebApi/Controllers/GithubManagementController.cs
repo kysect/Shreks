de377386ca6e06cc9d6883cea98f32b4ee01f727
Fredi Kats <fredikats@outlook.com>
2022-09-02 20:37:15 +0400

    feat: add controller for forcing github org updating

---
+++ Source/Kysect.Shreks.WebApi/Controllers/GithubManagementController.cs
@@ -0,0 +1,26 @@
+﻿using Kysect.Shreks.Application.Abstractions.Github.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kysect.Shreks.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GithubManagementController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public GithubManagementController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("force-update")]
+        public async Task<ActionResult> ForceOrganizationUpdate()
+        {
+            var updateOrganizationCommand = new UpdateSubjectCourseOrganizations.Command();
+            await _mediator.Send(updateOrganizationCommand);
+            return Ok();
+        }
+    }
+}
