992556402175f9b828678cd6a3f35c7780aadc37
annchous <annchooousss@gmail.com>
2022-09-02 21:27:44 +0400

    feat: add get by id query and handler

---
+++ Source/Application/Kysect.Shreks.Application.Abstractions/Study/Queries/GetGroupAssignmentsByStudyGroupId.cs
@@ -0,0 +1,11 @@
+﻿using Kysect.Shreks.Application.Dto.Study;
+using MediatR;
+
+namespace Kysect.Shreks.Application.Abstractions.Study.Queries;
+
+public static class GetGroupAssignmentsByStudyGroupId
+{
+    public record Query(Guid GroupId) : IRequest<Response>;
+
+    public record Response(IReadOnlyCollection<GroupAssignmentDto> GroupAssignments);
+}
