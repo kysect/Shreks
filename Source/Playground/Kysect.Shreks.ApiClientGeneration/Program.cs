02f1042a04ad288d76db2195b481099765fe764c
bibletoon <alex8.ivanov@gmail.com>
2022-09-02 20:39:51 +0300

    Feat: add api client generator

---
+++ Source/Playground/Kysect.Shreks.ApiClientGeneration/Program.cs
@@ -0,0 +1,25 @@
+﻿using NSwag;
+using NSwag.CodeGeneration.CSharp;
+
+string filePath = "ShreksApiClient.cs";
+
+var httpClient = new HttpClient();
+
+var spec = await httpClient.GetStringAsync("https://localhost:7188/swagger/v1/swagger.json");
+
+var document = await OpenApiDocument.FromJsonAsync(spec);
+
+var settings = new CSharpClientGeneratorSettings
+{
+    ClassName = "ShreksApiClient",
+    CSharpGeneratorSettings =
+    {
+        Namespace = "Shreks.ApiClient"
+    }
+};
+
+
+var generator = new CSharpClientGenerator(document, settings);
+var code = generator.GenerateFile();
+
+File.WriteAllText(filePath, code);
