cd40897cc154351fe86e19d512a212d209fc8077
Vladislav Grigoriev <vvllaadd99@yandex.ru>
2022-09-03 05:23:09 +0300

    Time fix

--- Source/Domain/Kysect.Shreks.Core/Tools/Calendar.cs
+++ Source/Domain/Kysect.Shreks.Core/Tools/Calendar.cs
@@ -4,7 +4,9 @@ public static class Calendar
 {
     private static readonly TimeZoneInfo TimeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById("Russian Standard Time");
     
-    public static DateTime CurrentDateTime => TimeZoneInfo.ConvertTime(DateTime.Now, TimeZoneInfo);
+    public static DateTime CurrentDateTime => DateTime.SpecifyKind(
+        TimeZoneInfo.ConvertTime(DateTime.Now, TimeZoneInfo), 
+        DateTimeKind.Local);
 
     public static DateOnly CurrentDate => DateOnly.FromDateTime(CurrentDateTime);
 }
