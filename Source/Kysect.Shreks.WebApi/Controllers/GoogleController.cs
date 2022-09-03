0bb625b91703867da0ff0859ec18bfad1e038393
Fredi Kats <fredikats@outlook.com>
2022-09-03 04:13:30 +0400

    feat: add method for force google sync

---
+++ Source/Kysect.Shreks.WebApi/Controllers/GoogleController.cs
@@ -0,0 +1,24 @@
+﻿using Kysect.Shreks.Integration.Google;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kysect.Shreks.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GoogleController : ControllerBase
+    {
+        private readonly TableUpdateQueue _tableUpdate;
+
+        public GoogleController(TableUpdateQueue tableUpdate)
+        {
+            _tableUpdate = tableUpdate;
+        }
+
+        [HttpPost("force-sync")]
+        public Task<ActionResult> ForceSubjectCourseTableSyncAsync(Guid subjectCourseId)
+        {
+            _tableUpdate.EnqueueCoursePointsUpdate(subjectCourseId);
+            return Task.FromResult<ActionResult>(Ok());
+        }
+    }
+}
