d5c22251be9b7d8077bddfd995bd35a64793eea4
Fredi Kats <fredikats@outlook.com>
2022-09-03 15:40:23 +0400

    feat: add prefix logger

---
+++ Source/Domain/Kysect.Shreks.Common/PrefixLoggerProxy.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Kysect.Shreks.Common;
+
+public class PrefixLoggerProxy : ILogger
+{
+    private readonly ILogger _logger;
+    private readonly string _prefix;
+
+    public PrefixLoggerProxy(ILogger logger, string prefix)
+    {
+        _logger = logger;
+        _prefix = prefix;
+    }
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+    {
+        _logger.Log<string>(logLevel, eventId, PrepareMessage(state), exception, (s, e) => $"{PrepareMessage(formatter(state, e))}");
+    }
+
+    public bool IsEnabled(LogLevel logLevel)
+    {
+        return _logger.IsEnabled(logLevel);
+    }
+
+    public IDisposable BeginScope<TState>(TState state)
+    {
+        return _logger.BeginScope(state);
+    }
+
+    private string PrepareMessage<TState>(TState state)
+    {
+        return $"[{_prefix}] {state?.ToString()}";
+    }
+}
+
+public class PrefixLoggerProxy<T> : ILogger<T>
+{
+    private readonly ILogger _logger;
+    private readonly string _prefix;
+
+    public PrefixLoggerProxy(ILogger logger, string prefix)
+    {
+        _logger = logger;
+        _prefix = prefix;
+    }
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+    {
+        _logger.Log<string>(logLevel, eventId, PrepareMessage(state), exception, (s, e) => $"{PrepareMessage(formatter(state, e))}");
+    }
+
+    public bool IsEnabled(LogLevel logLevel)
+    {
+        return _logger.IsEnabled(logLevel);
+    }
+
+    public IDisposable BeginScope<TState>(TState state)
+    {
+        return _logger.BeginScope(state);
+    }
+
+    private string PrepareMessage<TState>(TState state)
+    {
+        return $"[{_prefix}] {state?.ToString()}";
+    }
+}
