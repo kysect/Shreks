fb63f1c0cfbaf62e868ea7aa3bd3703889f46d2c
Fredi Kats <fredikats@outlook.com>
2022-09-03 05:26:37 +0400

    feat: add subject course association mapping

---
+++ Source/Infrastructure/Kysect.Shreks.Mapping/Profiles/SubjectCourserProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Kysect.Shreks.Application.Dto.Study;
+using Kysect.Shreks.Core.SubjectCourseAssociations;
+
+namespace Kysect.Shreks.Mapping.Profiles
+{
+    internal class SubjectCourserProfile : Profile
+    {
+        public SubjectCourserProfile()
+        {
+            CreateMap<SubjectCourseAssociation, SubjectCourseAssociationDto>();
+        }
+    }
+}
