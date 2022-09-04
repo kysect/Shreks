105f4c2a16254fd7223baf49f401ab9d7a08c382
bibletoon <alex8.ivanov@gmail.com>
2022-09-04 17:27:27 +0300

    Fix: date time bug

--- Source/Domain/Kysect.Shreks.Core/Queue/Evaluators/SubmissionDateTimeEvaluator.cs
+++ Source/Domain/Kysect.Shreks.Core/Queue/Evaluators/SubmissionDateTimeEvaluator.cs
@@ -8,5 +8,5 @@ public partial class SubmissionDateTimeEvaluator : SubmissionEvaluator
     public SubmissionDateTimeEvaluator(int position, SortingOrder sortingOrder) : base(position, sortingOrder) { }
 
     public override double Evaluate(Submission submission)
-        => submission.SubmissionDate.Ticks;
+        => submission.SubmissionDate.Value.Ticks;
 }
--- Source/Domain/Kysect.Shreks.Core/Tools/Calendar.cs
+++ Source/Domain/Kysect.Shreks.Core/Tools/Calendar.cs
@@ -4,10 +4,16 @@ public static class Calendar
 {
     private static readonly TimeZoneInfo TimeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById("Russian Standard Time");
 
-    public static DateTime CurrentDateTime => DateTime.SpecifyKind(Convert(DateTime.Now), DateTimeKind.Local);
+    public static SpbDateTime CurrentDateTime => new SpbDateTime(DateTime.SpecifyKind(FromLocal(DateTime.Now).Value, DateTimeKind.Unspecified));
 
-    public static DateOnly CurrentDate => DateOnly.FromDateTime(CurrentDateTime);
+    public static DateOnly CurrentDate => DateOnly.FromDateTime(CurrentDateTime.Value);
 
-    public static DateTime Convert(DateTime dateTime)
-        => TimeZoneInfo.ConvertTime(dateTime, TimeZoneInfo);
+    public static SpbDateTime FromLocal(DateTime dateTime)
+        => new SpbDateTime(TimeZoneInfo.ConvertTime(dateTime, TimeZoneInfo));
+
+    public static SpbDateTime FromUtc(DateTime dateTime)
+        => new SpbDateTime(TimeZoneInfo.ConvertTimeFromUtc(dateTime, TimeZoneInfo));
+
+    public static DateTime ToUtc(SpbDateTime dateTime)
+        => TimeZoneInfo.ConvertTime(dateTime.Value, TimeZoneInfo, TimeZoneInfo.Utc);
 }
