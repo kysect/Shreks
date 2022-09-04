5e6f7ac7ed1d307898988bebd30f848b91a03d71
Fredi Kats <fredikats@outlook.com>
2022-09-04 17:31:21 +0400

    refactor: remove duplicated unused code

--- Source/Domain/Kysect.Shreks.Common/Logging/PrefixLoggerProxy.cs
+++ Source/Domain/Kysect.Shreks.Common/Logging/PrefixLoggerProxy.cs
@@ -33,35 +33,3 @@ public class PrefixLoggerProxy : ILogger
         return $"[{_prefix}] {state?.ToString()}";
     }
 }
-
-public class PrefixLoggerProxy<T> : ILogger<T>
-{
-    private readonly ILogger _logger;
-    private readonly string _prefix;
-
-    public PrefixLoggerProxy(ILogger logger, string prefix)
-    {
-        _logger = logger;
-        _prefix = prefix;
-    }
-
-    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
-    {
-        _logger.Log(logLevel, eventId, PrepareMessage(state), exception, (s, e) => $"{PrepareMessage(formatter(state, e))}");
-    }
-
-    public bool IsEnabled(LogLevel logLevel)
-    {
-        return _logger.IsEnabled(logLevel);
-    }
-
-    public IDisposable BeginScope<TState>(TState state)
-    {
-        return _logger.BeginScope(state);
-    }
-
-    private string PrepareMessage<TState>(TState state)
-    {
-        return $"[{_prefix}] {state?.ToString()}";
-    }
-}
