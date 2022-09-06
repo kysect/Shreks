de29f461af0e2a2214d0edc3c6898896d20cb846
Roman Makarevich <remmybump@gmail.com>
2022-09-06 17:32:09 +0300

    Revert "Refactor: max by usage"
    This reverts commit ae08ef38b236aecd00014163b29776da832ffb8b.

--- Source/Application/Kysect.Shreks.Application.Handlers/Github/GetLastSubmissionByPrHandler.cs
+++ Source/Application/Kysect.Shreks.Application.Handlers/Github/GetLastSubmissionByPrHandler.cs
@@ -22,14 +22,15 @@ public class GetLastSubmissionByPrHandler : IRequestHandler<Query, Response>
 
     public async Task<Response> Handle(Query request, CancellationToken cancellationToken)
     {
-        var submission = _context.SubmissionAssociations
+        var submission = await _context.SubmissionAssociations
             .OfType<GithubSubmissionAssociation>()
             .Where(a =>
                 a.Organization == request.PullRequestDescriptor.Organization
                 && a.Repository == request.PullRequestDescriptor.Repository
                 && a.PrNumber == request.PullRequestDescriptor.PrNumber)
-            .Select(s => s.Submission)
-            .MaxBy(s => s.SubmissionDate);
+            .OrderByDescending(a => a.Submission.SubmissionDate)
+            .Select(a => a.Submission)
+            .FirstOrDefaultAsync(cancellationToken);
 
         if (submission is null)
         {
