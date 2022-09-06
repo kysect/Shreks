b2e263c2514e1395f0509dd5514ef1b82bd3b792
Roman Makarevich <remmybump@gmail.com>
2022-09-06 19:35:58 +0300

    feat: Configured Serilog to use separate rolling files for requests and actual logs.

---
+++ Source/Kysect.Shreks.WebApi/Extensions/ConfigureHostBuilderExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Serilog;
+using Serilog.Events;
+using static Kysect.Shreks.WebApi.Configuration.LoggerPathConfiguration;
+
+namespace Kysect.Shreks.WebApi.Extensions;
+
+public static class ConfigureHostBuilderExtensions {
+    public static IHostBuilder UseSerilogForAppLogs(this ConfigureHostBuilder hostBuilder)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Verbose()
+            .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
+            .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+            .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
+            .WriteTo
+                .Console(outputTemplate: "[{Timestamp:T} {Level:u3}] ({SourceContext}) {Message}{NewLine}{Exception}")
+            .WriteTo
+                .File(Path.Join(BaseDirectoryName, "AppLogs_.log"),
+                    outputTemplate: "{Timestamp:o} [{Level:u3}] ({SourceContext}) {Message}{NewLine}{Exception}",
+                    rollingInterval: RollingInterval.Day,
+                    retainedFileCountLimit: 30
+                )
+            .CreateLogger();
+
+        return hostBuilder.UseSerilog();
+    }
+}
