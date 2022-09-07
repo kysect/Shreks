157427a262a68db7a02278cb57406d417b014e28
Fredi Kats <fredikats@outlook.com>
2022-09-08 01:02:23 +0400

    fix: add missed DI registration

--- Source/Application/Kysect.Shreks.Application.GithubWorkflow/Extensions/ServiceCollectionExtensions.cs
+++ Source/Application/Kysect.Shreks.Application.GithubWorkflow/Extensions/ServiceCollectionExtensions.cs
@@ -1,12 +1,14 @@
+using Kysect.Shreks.Application.GithubWorkflow.Abstractions;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Kysect.Shreks.Application.GithubWorkflow.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    private static IServiceCollection AddGithubInviteBackgroundService(this IServiceCollection services)
+    public static IServiceCollection AddGithubInviteBackgroundService(this IServiceCollection services)
     {
         return services
+            .AddScoped<IShreksWebhookEventProcessor, ShreksWebhookEventProcessor>()
             .AddHostedService<GithubInvitingWorker>();
     }
 }
--- Source/Kysect.Shreks.WebApi/Program.cs
+++ Source/Kysect.Shreks.WebApi/Program.cs
@@ -2,6 +2,7 @@ using Google.Apis.Auth.OAuth2;
 using Kysect.Shreks.Application.Abstractions.Identity.Commands;
 using Kysect.Shreks.Application.Commands.Extensions;
 using Kysect.Shreks.Application.Extensions;
+using Kysect.Shreks.Application.GithubWorkflow.Extensions;
 using Kysect.Shreks.Application.Handlers.Extensions;
 using Kysect.Shreks.Common.Exceptions;
 using Kysect.Shreks.Core.Study;
@@ -107,7 +108,9 @@ void InitServiceCollection(WebApplicationBuilder webApplicationBuilder)
                 .ConfigureDriveId(googleIntegrationConfiguration.GoogleDriveId));
     }
 
-    webApplicationBuilder.Services.AddGithubServices(cacheConfiguration, githubIntegrationConfiguration);
+    webApplicationBuilder.Services
+        .AddGithubServices(cacheConfiguration, githubIntegrationConfiguration)
+        .AddGithubInviteBackgroundService();
 
     if (webApplicationBuilder.Environment.IsDevelopment())
     {
