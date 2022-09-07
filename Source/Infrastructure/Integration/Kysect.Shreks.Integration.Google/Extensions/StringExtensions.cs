6b4b28104bdfe3dab912d3a3d8fcfb0841804443
Mikhail Koshkin <70411602+Mihinator3000@users.noreply.github.com>
2022-09-07 10:50:40 +0300

    fix: add regex compiled options
    Co-authored-by: ronimizy <ronimizy@outlook.com>

--- Source/Infrastructure/Integration/Kysect.Shreks.Integration.Google/Extensions/StringExtensions.cs
+++ Source/Infrastructure/Integration/Kysect.Shreks.Integration.Google/Extensions/StringExtensions.cs
@@ -4,7 +4,7 @@ namespace Kysect.Shreks.Integration.Google.Extensions;
 
 internal static class StringExtensions
 {
-    private static readonly Regex HasCyrillicRegex = new(@"\p{IsCyrillic}");
+    private static readonly Regex HasCyrillicRegex = new(@"\p{IsCyrillic}", RegexOptions.Compiled);
 
     public static bool HasCyrillic(this string s)
         => HasCyrillicRegex.IsMatch(s);
