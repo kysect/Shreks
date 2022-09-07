3cf729426358068c08668634d9c8bfbb0ed4d988
Vladislav Grigoriev <vvllaadd99@yandex.ru>
2022-09-07 19:40:28 +0300

    update port

--- YaCloud/start_test.py
+++ YaCloud/start_test.py
@@ -2,4 +2,4 @@
 
 from start import start_container
 
-start_container('shreks-test', 'e6q53tol0qk89oe61tgu', 5068, 'Staging')
+start_container('shreks-test', 'e6q53tol0qk89oe61tgu', 3045, 'Staging')
