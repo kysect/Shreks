0212afd11d105ade56b53d699b42005fa5506ba7
Fredi Kats <fredikats@outlook.com>
2022-09-08 22:13:47 +0300

    Update Source/Domain/Kysect.Shreks.Common/Exceptions/EntityNotFoundException.cs
    Co-authored-by: Annchous <57408376+annchous@users.noreply.github.com>

--- Source/Domain/Kysect.Shreks.Common/Exceptions/EntityNotFoundException.cs
+++ Source/Domain/Kysect.Shreks.Common/Exceptions/EntityNotFoundException.cs
@@ -5,7 +5,7 @@ public class EntityNotFoundException : NotFoundException
 
     public static EntityNotFoundException For<T>(Guid id)
     {
-        return new EntityNotFoundException($"Entity of type {typeof(T).Name} with id {id} not found");
+        return new EntityNotFoundException($"Entity of type {typeof(T).Name} with id {id} was not found");
     }
 
     public EntityNotFoundException(string? message) : base(message) { }
