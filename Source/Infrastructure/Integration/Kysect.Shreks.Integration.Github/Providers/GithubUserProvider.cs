31668c552557a0662016aa9356c9d5307aa65134
annchous <annchooousss@gmail.com>
2022-09-13 23:08:18 +0400

    feat: add github user provider

---
+++ Source/Infrastructure/Integration/Kysect.Shreks.Integration.Github/Providers/GithubUserProvider.cs
@@ -0,0 +1,29 @@
+﻿using Kysect.Shreks.Application.GithubWorkflow.Abstractions;
+using Octokit;
+
+namespace Kysect.Shreks.Integration.Github.Providers;
+
+public class GithubUserProvider : IGithubUserProvider
+{
+    private readonly IGitHubClient _appClient;
+
+    public GithubUserProvider(IGitHubClient appClient)
+    {
+        _appClient = appClient;
+    }
+
+    public async Task<bool> IsGithubUserExists(string username)
+    {
+        ArgumentNullException.ThrowIfNull(username);
+
+        try
+        {
+            await _appClient.User.Get(username);
+            return true;
+        }
+        catch (NotFoundException)
+        {
+            return false;
+        }
+    }
+}
