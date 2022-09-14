3ad30baf7ccea902003beb89e6255df76d53d265
Fredi Kats <fredikats@outlook.com>
2022-09-14 22:41:21 +0400

    feat: add SubmissionStateWorkflowType

--- Source/Application/Kysect.Shreks.Application.Abstractions/Study/Commands/CreateSubjectCourse.cs
+++ Source/Application/Kysect.Shreks.Application.Abstractions/Study/Commands/CreateSubjectCourse.cs
@@ -5,7 +5,7 @@ namespace Kysect.Shreks.Application.Abstractions.Study.Commands;
 
 public static class CreateSubjectCourse
 {
-    public record Command(Guid SubjectId, string Title) : IRequest<Response>;
+    public record Command(Guid SubjectId, string Title, SubmissionStateWorkflowTypeDto WorkflowType) : IRequest<Response>;
 
     public record Response(SubjectCourseDto SubjectCourse);
 }
