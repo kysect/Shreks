ad0f4dc4f7f169c974ed0c91dff9ec197ff97071
Fredi Kats <fredikats@outlook.com>
2022-09-15 22:16:59 +0400

    Add migration for subject workflow type

---
+++ Source/Infrastructure/Kysect.Shreks.DataAccess/Migrations/20220915180822_Subject-workflow-type-added.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kysect.Shreks.DataAccess.Migrations
+{
+    public partial class Subjectworkflowtypeadded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "WorkflowType",
+                table: "SubjectCourses",
+                type: "integer",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WorkflowType",
+                table: "SubjectCourses");
+        }
+    }
+}
