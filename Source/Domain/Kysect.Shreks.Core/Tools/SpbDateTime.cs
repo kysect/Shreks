dba3c03d7171f5b06db4fdabff3f0a3252ff3c4d
Fredi Kats <fredikats@outlook.com>
2022-09-17 00:23:21 +0400

    fix: write date time in re local

--- Source/Domain/Kysect.Shreks.Core/Tools/SpbDateTime.cs
+++ Source/Domain/Kysect.Shreks.Core/Tools/SpbDateTime.cs
@@ -6,4 +6,14 @@ public record struct SpbDateTime(DateTime Value)
     {
         return new SpbDateTime(dateOnly.ToDateTime(TimeOnly.MinValue));
     }
+
+    public override string ToString()
+    {
+        return ToUserFriendlyString();
+    }
+
+    public string ToUserFriendlyString()
+    {
+        return Value.ToString("dd.MM.yyyy");
+    }
 }
