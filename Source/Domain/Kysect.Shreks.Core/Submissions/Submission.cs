cb16846c1dae0e2a83b2c0054b33d168e53638ed
Fredi Kats <fredikats@outlook.com>
2022-09-17 00:00:10 +0300

    Update Source/Domain/Kysect.Shreks.Core/Submissions/Submission.cs
    Co-authored-by: ronimizy <ronimizy@outlook.com>

--- Source/Domain/Kysect.Shreks.Core/Submissions/Submission.cs
+++ Source/Domain/Kysect.Shreks.Core/Submissions/Submission.cs
@@ -66,10 +66,7 @@ public abstract partial class Submission : IEntity<Guid>
 
     public void Rate(Fraction? rating, Points? extraPoints)
     {
-        if (State
-            is not SubmissionState.Active
-            and not SubmissionState.Reviewed
-            and not SubmissionState.Completed)
+        if (State is not (SubmissionState.Active or SubmissionState.Reviewed or SubmissionState.Completed))
         {
             string message = $"Cannot update submission points. Submission state: {State}.";
             throw new DomainInvalidOperationException(message);
