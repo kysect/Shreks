2125ba979fc9f50e48246b9a6844bd1d67df6eef
Fredi Kats <fredikats@outlook.com>
2022-09-25 00:17:24 +0400

    Add user messages to resources

---
+++ Source/Domain/Kysect.Shreks.Common/Resources/UserCommandProcessingMessage.cs
@@ -0,0 +1,49 @@
+﻿namespace Kysect.Shreks.Common.Resources;
+
+public static class UserCommandProcessingMessage
+{
+    public static string DomainExceptionWhileProcessingUserCommand(string commandName)
+    {
+        return string.Format(UserMessages.DomainExceptionWhileProcessingUserCommand, commandName);
+    }
+
+    public static string InternalExceptionWhileProcessingUserCommand()
+    {
+        return string.Format(UserMessages.InternalExceptionWhileProcessingUserCommand);
+    }
+
+    public static string SubmissionActivatedSuccessfully()
+    {
+        return string.Format(UserMessages.SubmissionActivatedSuccessfully);
+    }
+
+    public static string SubmissionCreated(string submissionDetails)
+    {
+        return string.Format(UserMessages.SubmissionCreated, submissionDetails);
+    }
+
+    public static string SubmissionDeactivatedSuccessfully()
+    {
+        return string.Format(UserMessages.SubmissionDeactivatedSuccessfully);
+    }
+
+    public static string SubmissionDeletedSuccessfully()
+    {
+        return string.Format(UserMessages.SubmissionDeletedSuccessfully);
+    }
+
+    public static string SubmissionRated(string submissionDetails)
+    {
+        return string.Format(UserMessages.SubmissionRated, submissionDetails);
+    }
+
+    public static string SubmissionUpdated(string submissionDetails)
+    {
+        return string.Format(UserMessages.SubmissionUpdated, submissionDetails);
+    }
+
+    public static string SubmissionMarkedAsReviewed()
+    {
+        return string.Format(UserMessages.SubmissionMarkedAsReviewed);
+    }
+}
