3c5a1631014f45aab481c40e1de0c89fb4dc7554
Fredi Kats <fredikats@outlook.com>
2022-10-01 13:41:18 +0400

    Move dev seeding to separated project and remove reference from Application to Infrastructure

---
+++ Source/DeveloperEnvironment/Kysect.Shreks.DeveloperEnvironment/DeveloperEnvironmentSeedingRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Kysect.Shreks.DeveloperEnvironment;
+
+public record DeveloperEnvironmentSeedingRequest(
+    string Environment,
+    string Organization,
+    string TemplateRepository,
+    IReadOnlyList<string> Users);
---
+++ Source/DeveloperEnvironment/Kysect.Shreks.DeveloperEnvironment/UserNotAcknowledgedEnvironmentException.cs
@@ -0,0 +1,9 @@
+﻿namespace Kysect.Shreks.DeveloperEnvironment;
+
+public class UserNotAcknowledgedEnvironmentException : InvalidOperationException
+{
+    public UserNotAcknowledgedEnvironmentException()
+        : base("You must ensure that is it a right environment to execute SeedTestData command")
+    {
+    }
+}
