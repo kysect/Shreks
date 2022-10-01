c500cc2fe009195df9b0bb1d028dfdb4e4b7e107
Fredi Kats <fredikats@outlook.com>
2022-10-01 10:05:00 +0400

    Test submission changes on pull request updating

--- Source/Tests/Kysect.Shreks.Tests/GithubWorkflow/GithubSubjectCourseAssociationTest.cs
+++ Source/Tests/Kysect.Shreks.Tests/GithubWorkflow/GithubSubjectCourseAssociationTest.cs
@@ -2,6 +2,7 @@
 using Kysect.Shreks.Application.Commands.Parsers;
 using Kysect.Shreks.Application.DatabaseContextExtensions;
 using Kysect.Shreks.Application.GithubWorkflow;
+using Kysect.Shreks.Application.GithubWorkflow.Abstractions;
 using Kysect.Shreks.Application.GithubWorkflow.Abstractions.Models;
 using Kysect.Shreks.Application.GithubWorkflow.Submissions;
 using Kysect.Shreks.Application.TableManagement;
@@ -9,6 +10,7 @@ using Kysect.Shreks.Core.Models;
 using Kysect.Shreks.Core.Study;
 using Kysect.Shreks.Core.SubjectCourseAssociations;
 using Kysect.Shreks.Core.Submissions;
+using Kysect.Shreks.Core.Tools;
 using Kysect.Shreks.Core.UserAssociations;
 using Kysect.Shreks.Core.Users;
 using Kysect.Shreks.Tests.GithubWorkflow.Tools;
@@ -48,7 +50,7 @@ public class GithubSubjectCourseAssociationTest : GithubWorkflowTestBase
     {
         var pullRequestCommitEventNotifier = new TestEventNotifier();
         var githubSubmissionService = new GithubSubmissionService(Context);
-        ShreksWebhookEventProcessor githubSubmissionStateMachine = CreateEventProcessor();
+        IShreksWebhookEventProcessor githubSubmissionStateMachine = CreateEventProcessor();
 
         (GithubSubjectCourseAssociation subjectCourseAssociation, Student student) = await TestContextGenerator.Create();
         GithubUserAssociation githubUserAssociation = student.User.Associations.OfType<GithubUserAssociation>().First();
@@ -67,6 +69,37 @@ public class GithubSubjectCourseAssociationTest : GithubWorkflowTestBase
         Submission lastSubmissionByPr = await githubSubmissionService.GetLastSubmissionByPr(githubPullRequestDescriptor);
 
         Assert.Equal(SubmissionState.Active, lastSubmissionByPr.State);
+    }
+
+    [Fact]
+    public async Task PullRequestUpdate_AfterSubmissionWasCreated_SubmissionShouldUpdateTime()
+    {
+        var pullRequestCommitEventNotifier = new TestEventNotifier();
+        var githubSubmissionService = new GithubSubmissionService(Context);
+        IShreksWebhookEventProcessor githubSubmissionStateMachine = CreateEventProcessor();
+
+        (GithubSubjectCourseAssociation subjectCourseAssociation, Student student) = await TestContextGenerator.Create();
+        GithubUserAssociation githubUserAssociation = student.User.Associations.OfType<GithubUserAssociation>().First();
+        Assignment assignment = subjectCourseAssociation.SubjectCourse.Assignments.First();
+
+        var githubPullRequestDescriptor = new GithubPullRequestDescriptor(
+            githubUserAssociation.GithubUsername,
+            string.Empty,
+            subjectCourseAssociation.GithubOrganizationName,
+            githubUserAssociation.GithubUsername,
+            assignment.ShortName,
+            1);
+
+        await githubSubmissionStateMachine.ProcessPullRequestUpdate(githubPullRequestDescriptor, _logger, pullRequestCommitEventNotifier, CancellationToken.None);
+        Submission createdSubmission = await githubSubmissionService.GetLastSubmissionByPr(githubPullRequestDescriptor);
+        // Do not inline, it lead to test failing
+        SpbDateTime submissionCreatedTime = createdSubmission.SubmissionDate;
+
+        await githubSubmissionStateMachine.ProcessPullRequestUpdate(githubPullRequestDescriptor, _logger, pullRequestCommitEventNotifier, CancellationToken.None);
+        Submission updatedSubmission = await githubSubmissionService.GetLastSubmissionByPr(githubPullRequestDescriptor);
+
+        Assert.Equal(updatedSubmission.Id, createdSubmission.Id);
+        Assert.NotEqual(updatedSubmission.SubmissionDate, submissionCreatedTime);
     }
 
     public ShreksWebhookEventProcessor CreateEventProcessor()
