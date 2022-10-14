8ccd5cd1436ca1878f1831b9ba70915288771b95
ronimizy <ronimizy@outlook.com>
2022-10-14 21:26:45 +0300

    chore: migrations

---
+++ Source/Infrastructure/Kysect.Shreks.DataAccess/Migrations/20221014182624_AddedCompromisedSubmission.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kysect.Shreks.DataAccess.Migrations
+{
+    public partial class AddedCompromisedSubmission : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCompromised",
+                table: "Submissions",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCompromised",
+                table: "Submissions");
+        }
+    }
+}
