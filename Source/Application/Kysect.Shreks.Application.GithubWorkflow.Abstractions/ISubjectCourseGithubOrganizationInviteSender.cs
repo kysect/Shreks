be100dd4b6a4d8f15a04a4636b25f5217f469bd3
Roman Makarevich <remmybump@gmail.com>
2023-01-04 02:33:58 +0300

    SA1402 File may only contain a single type

--- Source/Application/Kysect.Shreks.Application.GithubWorkflow.Abstractions/ISubjectCourseGithubOrganizationInviteSender.cs
+++ Source/Application/Kysect.Shreks.Application.GithubWorkflow.Abstractions/ISubjectCourseGithubOrganizationInviteSender.cs
@@ -1,10 +1,5 @@
 ﻿namespace Kysect.Shreks.Application.GithubWorkflow.Abstractions;
 
-public interface ISubjectCourseGithubOrganizationManager
-{
-    Task UpdateOrganizations(CancellationToken cancellationToken);
-}
-
 public interface ISubjectCourseGithubOrganizationInviteSender
 {
     Task Invite(string organizationName, IReadOnlyCollection<string> usernames);
